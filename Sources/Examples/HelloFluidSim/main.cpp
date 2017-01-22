e1b7a7e7a778f62f77d7a4af68148eff60ad7ffe
Chris Ohk <utilforever@gmail.com>
2017-01-22 15:39:34 +0900

    Computing motion

--- Sources/Examples/HelloFluidSim/main.cpp
+++ Sources/Examples/HelloFluidSim/main.cpp
@@ -1,11 +1,25 @@
 #include <cstdio>
 
+void UpdateWave(const double timeInterval, double* x, double* speed)
+{
+	(*x) += timeInterval * (*speed);
+}
+
 int main()
 {
 	double x = 0.0;
 	double y = 0.0;
 	double speedX = 1.0;
 	double speedY = -0.5;
+
+	const int fps = 100;
+	const double timeInterval = 1.0 / fps;
+
+	for (int i = 0; i < 1000; ++i)
+	{
+		// Update waves
+		UpdateWave(timeInterval, &x, &speedX);
+	}
 
 	return 0;
 }
