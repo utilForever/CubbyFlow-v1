b47723d9341aee04f8f80df3e76c475efbb1a13c
Chris Ohk <utilforever@gmail.com>
2017-01-23 13:02:11 +0900

    Add comments for Doxygen

--- Sources/Array/Array.h
+++ Sources/Array/Array.h
@@ -11,6 +11,17 @@
 
 namespace CubbyFlow
 {
+	//!
+	//! \brief Generic N-dimensional array class interface.
+	//!
+	//! This class provides generic template class for N-dimensional array where N
+	//! must be either 1, 2 or 3. This particular class exists to provide generic
+	//! interface for 1, 2 or 3 dimensional arrays using template specialization
+	//! only, but it cannot create any instance by itself.
+	//!
+	//! \tparam T - Real number type.
+	//! \tparam N - Dimension.
+	//!
 	template <typename T, size_t N>
 	class Array final
 	{
