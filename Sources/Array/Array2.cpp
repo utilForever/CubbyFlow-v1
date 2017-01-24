6db0d7670b205a8a69edbecf1842d7846b8c1dd7
Chris Ohk <utilforever@gmail.com>
2017-01-25 02:23:36 +0900

    Update Array2.cpp - 2-D array class

--- Sources/Array/Array2.cpp
+++ Sources/Array/Array2.cpp
@@ -6,3 +6,39 @@
 > Created Time: 2017/01/25
 > Copyright (c) 2017, Chan-Ho Chris Ohk
 *************************************************************************/
+#include <Array/Array2.h>
+
+#include <cassert>
+
+namespace CubbyFlow
+{
+	template <typename T>
+	Array<T, 2>::Array()
+	{
+		// Do nothing
+	}
+
+	template <typename T>
+	Array<T, 2>::Array(const Size2& size, const T& initVal)
+	{
+		Resize(size, initVal);
+	}
+
+	template <typename T>
+	Array<T, 2>::Array(size_t width, size_t height, const T& initVal)
+	{
+		Resize(width, height, initVal);
+	}
+
+	template <typename T>
+	Array<T, 2>::Array(const std::initializer_list<std::initializer_list<T>>& list)
+	{
+		Set(list);
+	}
+
+	template <typename T>
+	Array<T, 2>::Array(const Array& other)
+	{
+		Set(other);
+	}
+}
