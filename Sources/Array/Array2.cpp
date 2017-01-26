7d0a6d8b68ffe538bda6c5f406235cd46bd6d1c0
Chris Ohk <utilforever@gmail.com>
2017-01-26 23:08:58 +0900

    Update Array2.cpp - 2-D array class

--- Sources/Array/Array2.cpp
+++ Sources/Array/Array2.cpp
@@ -155,4 +155,178 @@ namespace CubbyFlow
 		assert(i < m_size.x && j < m_size.y);
 		return m_data[i + m_size.x * j];
 	}
+
+	template <typename T>
+	Size2 Array<T, 2>::Size() const
+	{
+		return m_size;
+	}
+
+	template <typename T>
+	size_t Array<T, 2>::Width() const
+	{
+		return m_size.x;
+	}
+
+	template <typename T>
+	size_t Array<T, 2>::Height() const
+	{
+		return m_size.y;
+	}
+
+	template <typename T>
+	T* Array<T, 2>::Data()
+	{
+		return m_data.data();
+	}
+
+	template <typename T>
+	const T* Array<T, 2>::Data() const
+	{
+		return m_data.data();
+	}
+
+	template <typename T>
+	typename Array<T, 2>::ContainerType::iterator Array<T, 2>::Begin()
+	{
+		return m_data.begin();
+	}
+
+	template <typename T>
+	typename Array<T, 2>::ContainerType::const_iterator Array<T, 2>::Begin() const
+	{
+		return m_data.cbegin();
+	}
+
+	template <typename T>
+	typename Array<T, 2>::ContainerType::iterator Array<T, 2>::End()
+	{
+		return m_data.end();
+	}
+
+	template <typename T>
+	typename Array<T, 2>::ContainerType::const_iterator Array<T, 2>::End() const
+	{
+		return m_data.cend();
+	}
+
+	template <typename T>
+	ArrayAccessor2<T> Array<T, 2>::Accessor()
+	{
+		return ArrayAccessor2<T>(Size(), Data());
+	}
+
+	template <typename T>
+	ConstArrayAccessor2<T> Array<T, 2>::ConstAccessor() const
+	{
+		return ConstArrayAccessor2<T>(Size(), Data());
+	}
+
+	template <typename T>
+	void Array<T, 2>::Swap(Array& other)
+	{
+		std::swap(other.m_data, m_data);
+		std::swap(other.m_size, m_size);
+	}
+
+	template <typename T>
+	template <typename Callback>
+	void Array<T, 2>::ForEach(Callback func) const
+	{
+		ConstAccessor().ForEach(func);
+	}
+
+	template <typename T>
+	template <typename Callback>
+	void Array<T, 2>::ForEachIndex(Callback func) const
+	{
+		ConstAccessor().ForEachIndex(func);
+	}
+
+	template <typename T>
+	template <typename Callback>
+	void Array<T, 2>::ParallelForEach(Callback func)
+	{
+		Accessor().ParallelForEach(func);
+	}
+
+	template <typename T>
+	template <typename Callback>
+	void Array<T, 2>::ParallelForEachIndex(Callback func) const
+	{
+		ConstAccessor().ParallelForEachIndex(func);
+	}
+
+	template <typename T>
+	T& Array<T, 2>::operator[](size_t i)
+	{
+		return m_data[i];
+	}
+
+	template <typename T>
+	const T& Array<T, 2>::operator[](size_t i) const
+	{
+		return m_data[i];
+	}
+
+	template <typename T>
+	T& Array<T, 2>::operator()(size_t i, size_t j)
+	{
+		assert(i < m_size.x && j < m_size.y);
+		return m_data[i + m_size.x * j];
+	}
+
+	template <typename T>
+	const T& Array<T, 2>::operator()(size_t i, size_t j) const
+	{
+		assert(i < m_size.x && j < m_size.y);
+		return m_data[i + m_size.x * j];
+	}
+
+	template <typename T>
+	T& Array<T, 2>::operator()(const Point2UI &pt)
+	{
+		assert(pt.x < m_size.x && pt.y < m_size.y);
+		return m_data[pt.x + m_size.x * pt.y];
+	}
+
+	template <typename T>
+	const T& Array<T, 2>::operator()(const Point2UI &pt) const
+	{
+		assert(pt.x < m_size.x && pt.y < m_size.y);
+		return m_data[pt.x + m_size.x * pt.y];
+	}
+
+	template <typename T>
+	Array<T, 2>& Array<T, 2>::operator=(const T& value)
+	{
+		Set(value);
+		return *this;
+	}
+
+	template <typename T>
+	Array<T, 2>& Array<T, 2>::operator=(const Array& other)
+	{
+		Set(other);
+		return *this;
+	}
+
+	template <typename T>
+	Array<T, 2>& Array<T, 2>::operator=(const std::initializer_list<std::initializer_list<T>>& list)
+	{
+		Set(list);
+		return *this;
+	}
+
+	template <typename T>
+	Array<T, 2>::operator ArrayAccessor2<T>()
+	{
+		return Accessor();
+	}
+
+	template <typename T>
+	Array<T, 2>::operator ConstArrayAccessor2<T>() const
+	{
+		return ConstAccessor();
+	}
 }
