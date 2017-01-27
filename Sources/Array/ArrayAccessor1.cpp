010bb9a7569b9ea43ee22df03f3207cb31a6fef4
Chris Ohk <utilforever@gmail.com>
2017-01-27 23:52:01 +0900

    Create ArrayAccessor1.cpp - 1-D array accessor class

---
+++ Sources/Array/ArrayAccessor1.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: ArrayAccessor1.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 1-D array accessor class.
+> Created Time: 2017/01/28
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
