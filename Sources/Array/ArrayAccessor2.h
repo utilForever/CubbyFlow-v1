9b66459a41bac47fe5a539c58daf672d259c1d10
Chris Ohk <utilforever@gmail.com>
2017-01-27 23:48:40 +0900

    Create ArrayAccessor2.h - 2-D array accessor class

---
+++ Sources/Array/ArrayAccessor2.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+> File Name: ArrayAccessor2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D array accessor class.
+> Created Time: 2017/01/28
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_ARRAY_ACCESSOR2_H
+#define CUBBYFLOW_ARRAY_ACCESSOR2_H
+
+#include <Array/ArrayAccessor.h>
+
+namespace CubbyFlow
+{
+}
+
+#endif
