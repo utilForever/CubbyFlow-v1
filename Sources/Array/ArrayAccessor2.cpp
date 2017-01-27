a8315dec967c998facd22a4348dd13476b5c2675
Chris Ohk <utilforever@gmail.com>
2017-01-27 23:54:46 +0900

    Create ArrayAccessor2.cpp - 2-D array accessor class

---
+++ Sources/Array/ArrayAccessor2.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: ArrayAccessor2.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D array accessor class.
+> Created Time: 2017/01/28
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
