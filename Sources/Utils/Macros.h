0c2ac789921a675154628049de3883dd5a674443
Chris Ohk <utilforever@gmail.com>
2017-01-31 19:25:59 +0900

    Create Macros.h - Macro lists for CubbyFlow

---
+++ Sources/Utils/Macros.h
@@ -0,0 +1,28 @@
+/*************************************************************************
+> File Name: Macros.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Macro lists for CubbyFlow.
+> Created Time: 2017/01/31
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_MACROS_H
+#define CUBBYFLOW_MACROS_H
+
+#if defined(_WIN32) || defined(_WIN64)
+#   define CUBBYFLOW_WINDOWS
+#elif defined(__APPLE__)
+#   define CUBBYFLOW_APPLE
+#   ifndef CUBBYFLOW_IOS
+#       define CUBBYFLOW_MACOSX
+#   endif
+#elif defined(linux) || defined(__linux__)
+#   define CUBBYFLOW_LINUX
+#endif
+
+#ifdef CUBBYFLOW_WINDOWS
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
+#endif
