a1b6c14e9f45a6f2d1a28a8d34c492a68ad5cda8
Chris Ohk <utilforever@gmail.com>
2017-02-01 02:02:21 +0900

    Update Point.h - Generic N-D point class

--- Sources/Point/Point.h
+++ Sources/Point/Point.h
@@ -9,6 +9,7 @@
 #ifndef CUBBYFLOW_POINT_H
 #define CUBBYFLOW_POINT_H
 
+#include <array>
 #include <type_traits>
 
 namespace CubbyFlow
@@ -24,6 +25,48 @@ namespace CubbyFlow
 	public:
 		static_assert(N > 0, "Size of static-sized point should be greater than zero.");
 		static_assert(std::is_arithmetic<T>::value, "Point only can be instantiated with arithmetic types");
+
+		//! Constructs a point with zeros.
+		Point();
+
+		//! Constructs point instance with parameters.
+		template <typename... Params>
+		explicit Point(Params... params);
+
+		//! Constructs point instance with initializer list.
+		template <typename U>
+		explicit Point(const std::initializer_list<U>& list);
+
+		//! Copy constructor.
+		Point(const Point& other);
+
+		//! Set point instance with initializer list.
+		template <typename U>
+		void Set(const std::initializer_list<U>& list);
+
+		//! Set point instance with other point.
+		void Set(const Point& other);
+
+		//! Set point instance with initializer list.
+		template <typename U>
+		Point& operator=(const std::initializer_list<U>& list);
+
+		//! Set point instance with other point.
+		Point& operator=(const Point& other);
+
+		//! Returns the const reference to the \p i -th element.
+		const T& operator[](size_t i) const;
+
+		//! Returns the reference to the \p i -th element.
+		T& operator[](size_t);
+
+	private:
+		std::array<T, N> m_elements;
+
+		template <typename... Params>
+		void SetAt(size_t i, T v, Params... params);
+
+		void SetAt(size_t i, T v);
 	};
 }
 
