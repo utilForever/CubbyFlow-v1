b9451d6df3721bd82c88c5eef750294831123e54
Chris Ohk <utilforever@gmail.com>
2017-01-31 19:26:18 +0900

    Create Constants.h - Constant lists for CubbyFlow

---
+++ Sources/Utils/Constants.h
@@ -0,0 +1,23 @@
+/*************************************************************************
+> File Name: Constants.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Constant lists for CubbyFlow.
+> Created Time: 2017/01/31
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_CONSTANTS_H
+#define CUBBYFLOW_CONSTANTS_H
+
+#include <Utils/Macros.h>
+
+namespace CubbyFlow
+{
+	// MARK: Zero
+	//! Zero size_t.
+	constexpr size_t ZERO_SIZE = 0;
+	//! Zero ssize_t.
+	constexpr ssize_t ZERO_SSIZE = 0;
+}
+
+#endif
