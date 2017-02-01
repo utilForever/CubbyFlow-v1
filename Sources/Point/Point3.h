0a99b6a4c10c9bbe1f85af4f47a23bb7ba90c1d1
Chris Ohk <utilforever@gmail.com>
2017-02-01 23:39:24 +0900

    Create Point3.h - 3-D point class

---
+++ Sources/Point/Point3.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+> File Name: Point3.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3-D point class.
+> Created Time: 2017/02/01
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_POINT3_H
+#define CUBBYFLOW_POINT3_H
+
+#include <Point/Point.h>
+
+namespace CubbyFlow
+{
+}
+
+#endif
