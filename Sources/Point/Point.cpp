c5eb9479b9f249c1f1450cf86133937ba6e3f267
Chris Ohk <utilforever@gmail.com>
2017-02-04 08:58:41 +0900

    Update Point.cpp - Generic N-D point class

--- Sources/Point/Point.cpp
+++ Sources/Point/Point.cpp
@@ -48,4 +48,66 @@ namespace CubbyFlow
 	{
 		// Do nothing
 	}
+
+	template <typename T, size_t N>
+	template <typename U>
+	void Point<T, N>::Set(const std::initializer_list<U>& list)
+	{
+		assert(list.size() >= N);
+
+		size_t i = 0;
+		for (const auto& inputElem : list)
+		{
+			m_elements[i] = static_cast<T>(inputElem);
+			++i;
+		}
+	}
+
+	template <typename T, size_t N>
+	void Point<T, N>::Set(const Point& other)
+	{
+		m_elements = other.m_elements;
+	}
+
+	template <typename T, size_t N>
+	template <typename U>
+	Point<T, N>& Point<T, N>::operator=(const std::initializer_list<U>& list)
+	{
+		Set(list);
+		return *this;
+	}
+
+	template <typename T, size_t N>
+	Point<T, N>& Point<T, N>::operator=(const Point& other)
+	{
+		Set(other);
+		return *this;
+	}
+
+	template <typename T, size_t N>
+	const T& Point<T, N>::operator[](size_t i) const
+	{
+		return m_elements[i];
+	}
+
+	template <typename T, size_t N>
+	T& Point<T, N>::operator[](size_t i)
+	{
+		return m_elements[i];
+	}
+
+	template <typename T, size_t N>
+	template <typename... Params>
+	void Point<T, N>::SetAt(size_t i, T v, Params... params)
+	{
+		m_elements[i] = v;
+
+		SetAt(i + 1, params...);
+	}
+
+	template <typename T, size_t N>
+	void Point<T, N>::SetAt(size_t i, T v)
+	{
+		m_elements[i] = v;
+	}
 }
