9a4933323b1f6c25aa67b7d3d9f847f8c510e9c7
Chris Ohk <utilforever@gmail.com>
2017-02-03 23:11:22 +0900

    Create Point2.cpp - 2-D point class

---
+++ Sources/Point/Point2.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: Point2.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D point class.
+> Created Time: 2017/02/03
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
