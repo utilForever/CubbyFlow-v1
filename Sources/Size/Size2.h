f549eb67d2c3d8fd0a79ca2e633c1b77a74b1a92
Chris Ohk <utilforever@gmail.com>
2017-02-04 18:08:57 +0900

    Create Size2 class

---
+++ Sources/Size/Size2.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+> File Name: Size2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D size type.
+> Created Time: 2017/02/04
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_SIZE2_H
+#define CUBBYFLOW_SIZE2_H
+
+#endif
