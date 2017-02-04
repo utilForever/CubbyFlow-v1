03475b19585dcfd9fbd16780463458c9f53b79b5
Chris Ohk <utilforever@gmail.com>
2017-02-04 18:08:49 +0900

    Create Size class

---
+++ Sources/Size/Size.cpp
@@ -0,0 +1,8 @@
+/*************************************************************************
+> File Name: Size.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: N-D size type.
+> Created Time: 2017/02/04
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
