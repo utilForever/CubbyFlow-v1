e0bfd8c388569bb67263cba3bd12d6afffa15802
Chris Ohk <utilforever@gmail.com>
2017-02-04 18:09:05 +0900

    Create Size3 class

---
+++ Sources/Size/Size3.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+> File Name: Size3.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3-D size type.
+> Created Time: 2017/02/04
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_SIZE3_H
+#define CUBBYFLOW_SIZE3_H
+
+#endif
