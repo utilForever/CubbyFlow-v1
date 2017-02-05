ada59bb72b788a14a4916864aea0ee4f5297612f
Chris Ohk <utilforever@gmail.com>
2017-02-05 15:25:17 +0900

    Delete unused cpp files

--- Sources/Size/Size.cpp
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
-> File Name: Size.cpp
-> Project Name: CubbyFlow
-> Author: Chan-Ho Chris Ohk
-> Purpose: N-D size type.
-> Created Time: 2017/02/04
-> Copyright (c) 2017, Chan-Ho Chris Ohk
-*************************************************************************/
--- Sources/Size/Size2.cpp
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
-> File Name: Size2.cpp
-> Project Name: CubbyFlow
-> Author: Chan-Ho Chris Ohk
-> Purpose: 2-D size type.
-> Created Time: 2017/02/04
-> Copyright (c) 2017, Chan-Ho Chris Ohk
-*************************************************************************/
