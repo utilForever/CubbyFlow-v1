6cab08d0116f9af023f9376f9647892575291640
Chris Ohk <utilforever@gmail.com>
2017-03-13 23:34:46 +0900

    Create Field2.h - Abstract base class for 2-D fields

---
+++ Sources/Field/Field2.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+> File Name: Field2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Abstract base class for 2-D fields.
+> Created Time: 2017/01/23
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_FIELD2_H
+#define CUBBYFLOW_FIELD2_H
+
+namespace CubbyFlow
+{
+	//! Abstract base class for 2-D fields.
+	class Field2 {
+	public:
+		Field2();
+		virtual ~Field2();
+	};
+}
+
+#endif
