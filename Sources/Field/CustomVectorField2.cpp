2d5eb048ec80662137850e30fe1b5753ea3c3b92
Chris Ohk <utilforever@gmail.com>
2017-03-24 23:36:15 +0900

    Update CustomVectorField2.cpp - Implement CustomVectorField2 methods

--- Sources/Field/CustomVectorField2.cpp
+++ Sources/Field/CustomVectorField2.cpp
@@ -10,5 +10,79 @@
 
 namespace CubbyFlow
 {
+	CustomVectorField2::CustomVectorField2(
+		const std::function<Vector2D(const Vector2D&)>& customFunction,
+		double derivativeResolution) :
+		m_customFunction(customFunction),
+		m_resolution(derivativeResolution)
+	{
+		// Do nothing
+	}
 
+	CustomVectorField2::CustomVectorField2(
+		const std::function<Vector2D(const Vector2D&)>& customFunction,
+		const std::function<double(const Vector2D&)>& customDivergenceFunction,
+		double derivativeResolution) :
+		m_customFunction(customFunction),
+		m_customDivergenceFunction(customDivergenceFunction),
+		m_resolution(derivativeResolution)
+	{
+		// Do nothing
+	}
+
+	CustomVectorField2::CustomVectorField2(
+		const std::function<Vector2D(const Vector2D&)>& customFunction,
+		const std::function<double(const Vector2D&)>& customDivergenceFunction,
+		const std::function<double(const Vector2D&)>& customCurlFunction) :
+		m_customFunction(customFunction),
+		m_customDivergenceFunction(customDivergenceFunction),
+		m_customCurlFunction(customCurlFunction)
+	{
+		// Do nothing
+	}
+
+	Vector2D CustomVectorField2::Sample(const Vector2D& x) const
+	{
+		return m_customFunction(x);
+	}
+
+	std::function<Vector2D(const Vector2D&)> CustomVectorField2::Sampler() const
+	{
+		return m_customFunction;
+	}
+
+	double CustomVectorField2::Divergence(const Vector2D& x) const
+	{
+		if (m_customDivergenceFunction)
+		{
+			return m_customDivergenceFunction(x);
+		}
+		
+		double left = m_customFunction(x - Vector2D(0.5 * m_resolution, 0.0)).x;
+		double right = m_customFunction(x + Vector2D(0.5 * m_resolution, 0.0)).x;
+		double bottom = m_customFunction(x - Vector2D(0.0, 0.5 * m_resolution)).y;
+		double top = m_customFunction(x + Vector2D(0.0, 0.5 * m_resolution)).y;
+
+		return (right - left + top - bottom) / m_resolution;
+	}
+
+	double CustomVectorField2::Curl(const Vector2D& x) const
+	{
+		if (m_customCurlFunction)
+		{
+			return m_customCurlFunction(x);
+		}
+	
+		double left = m_customFunction(x - Vector2D(0.5 * m_resolution, 0.0)).y;
+		double right = m_customFunction(x + Vector2D(0.5 * m_resolution, 0.0)).y;
+		double bottom = m_customFunction(x - Vector2D(0.0, 0.5 * m_resolution)).x;
+		double top = m_customFunction(x + Vector2D(0.0, 0.5 * m_resolution)).x;
+
+		return (top - bottom - right + left) / m_resolution;
+	}
+
+	CustomVectorField2::Builder CustomVectorField2::Builder()
+	{
+		return Builder();
+	}
 }
