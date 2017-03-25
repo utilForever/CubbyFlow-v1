39a99c052b07f8244a25b5362c7d012c9dff7634
Chris Ohk <utilforever@gmail.com>
2017-03-25 15:44:21 +0900

    Update Quaternion.cpp - Change Quaternion to Quaternion<T>

--- Sources/Math/Quaternion.cpp
+++ Sources/Math/Quaternion.cpp
@@ -6,74 +6,74 @@
 > Created Time: 2017/03/21
 > Copyright (c) 2017, Dongmin Kim
 *************************************************************************/
-#include<Math\Quaternion.h>
+#include <Math/Quaternion.h>
 
 namespace CubbyFlow
 {
 	template <typename T>
-	Quaternion::Quaternion()
+	Quaternion<T>::Quaternion()
 	{
 		// Do nothing
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(T newW, T newX, T newY, T newZ) 
+	Quaternion<T>::Quaternion(T newW, T newX, T newY, T newZ) 
 		: w(newW), x(newX), y(newY), z(newZ)
 	{
 		// Do nothing
 	}
 	
 	template <typename T>
-	Quaternion::Quaternion(const std::initializer_list<T>& list)
+	Quaternion<T>::Quaternion(const std::initializer_list<T>& list)
 	{
 		Set(list);
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(const Vector3<T>& axis, T angle)
+	Quaternion<T>::Quaternion(const Vector3<T>& axis, T angle)
 	{
 		Set(axis, angle);
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(const Vector3<T>& from, const Vector3<T>& to)
+	Quaternion<T>::Quaternion(const Vector3<T>& from, const Vector3<T>& to)
 	{
 		Set(from, to);
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(const Vector3<T>& axis0, const Vector3<T>& axis1, const Vector3<T>& axis2)
+	Quaternion<T>::Quaternion(const Vector3<T>& axis0, const Vector3<T>& axis1, const Vector3<T>& axis2)
 	{
 		Set(axis0, axis1, axis2);
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(const Matrix3x3<T>& m33)
+	Quaternion<T>::Quaternion(const Matrix3x3<T>& m33)
 	{
 		Set(m33);
 	}
 
 	template <typename T>
-	Quaternion::Quaternion(const Quaternion& other)
+	Quaternion<T>::Quaternion(const Quaternion& other)
 	{
 		Set(other);
 	}
 
 	template <typename T>
-	void Quaternion::Set(const Quaternion& other)
+	void Quaternion<T>::Set(const Quaternion& other)
 	{
 		Set(other.w, other.x, other.y, other.z);
 	}
 
 	template <typename T>
-	void Quaternion::Set(T newW, T newX, T newY, T newZ) 
+	void Quaternion<T>::Set(T newW, T newX, T newY, T newZ) 
 		: w(newW), x(newX), y(newY), z(newZ)
 	{
 		// Do nothing
 	}
 
 	template <typename T>
-	void Quaternion::Set(const std::initializer_list<T>& list)
+	void Quaternion<T>::Set(const std::initializer_list<T>& list)
 	{
 		assert(list.size() == 4);
 
@@ -85,13 +85,13 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	void Quaternion::Set(const Vector3<T>& axis, T angle)
+	void Quaternion<T>::Set(const Vector3<T>& axis, T angle)
 	{
 		Set(std::cos(angle / 2), axis.normalized() * std::sin(angle / 2));
 	}
 
 	template <typename T>
-	void Quaternion::Set(const Vector3<T>& from, const Vector3<T>& to)
+	void Quaternion<T>::Set(const Vector3<T>& from, const Vector3<T>& to)
 	{
 		T theta = acos(from.Dot(to) / sqrt(from.SquaredLength + to.SquaredLength));
 		Vector3<T> axis = from.Cross(to);
@@ -99,7 +99,7 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	void Quaternion::Set(const Matrix3x3<T>& matrix)
+	void Quaternion<T>::Set(const Matrix3x3<T>& matrix)
 	{
 		w = sqrt(1 + matrix(0, 0) + matrix(1, 1) + matrix(2, 2)) / 2;
 		x = (matrix(2, 1) - matrix(1, 2)) / (4 * w);
@@ -108,33 +108,33 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	void Quaternion::Set(const Vector3<T>& roationBasis0, const Vector3<T>& rotationBasis1, const Vector3<T>& rotationBasis2)
+	void Quaternion<T>::Set(const Vector3<T>& roationBasis0, const Vector3<T>& rotationBasis1, const Vector3<T>& rotationBasis2)
 	{
 
 	}
 
 	template <typename T>
 	template <typename U>
-	Quaternion<U> Quaternion::CastTo() const
+	Quaternion<U> Quaternion<T>::CastTo() const
 	{
 		return Quaternion<U>(static_cast<U>(w), static_cast<U>(x), static_cast<U>(y), static_cast<U>(z));
 	}
 
 	template <typename T>
-	Quaternion Quaternion::Normalized() const
+	Quaternion Quaternion<T>::Normalized() const
 	{
 		T Length = sqrt(w * w + x * x + y * y + z * z);
 		return Quaternion<T>(w / Length, x / Length, y / Length, z / Length);
 	}
 
 	template <typename T>
-	Vector3<T> Quaternion::Mul(const Vector3<T>& v) const
+	Vector3<T> Quaternion<T>::Mul(const Vector3<T>& v) const
 	{
 		return this * v * conjugate();
 	}
 
 	template <typename T>
-	Quaternion Quaternion::Mul(const Quaternion& other) const
+	Quaternion Quaternion<T>::Mul(const Quaternion& other) const
 	{
 		Vector3<T> v1 = Vector3<T>(x, y, z);
 		Vector3<T> v2 = Vector3<T>(other.x, other.y, other.z);
@@ -143,13 +143,13 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	T Quaternion::Dot(const Quaternion<T>& other)
+	T Quaternion<T>::Dot(const Quaternion<T>& other)
 	{
 		return w * other.w + x * other.x + y * other.y + z * other.z;
 	}
 
 	template <typename T>
-	Quaternion Quaternion::RMul(const Quaternion& other) const
+	Quaternion Quaternion<T>::RMul(const Quaternion& other) const
 	{
 		Vector3<T> v1 = Vector3<T>(x, y, z);
 		Vector3<T> v2 = Vector3<T>(other.x, other.y, other.z);
@@ -158,7 +158,7 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	void Quaternion::IMul(const Quaternion& other)
+	void Quaternion<T>::IMul(const Quaternion& other)
 	{
 		Vector3<T> v1 = Vector3<T>(x, y, z);
 		Vector3<T> v2 = Vector3<T>(other.x, other.y, other.z);
@@ -167,65 +167,65 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	void Quaternion::SetIdentity()
+	void Quaternion<T>::SetIdentity()
 	{
 		Set(1, 0, 0, 0);
 	}
 
 	template <typename T>
-	void Quaternion::Rotate(T angleInRadians)
+	void Quaternion<T>::Rotate(T angleInRadians)
 	{
 		Set(Axis(), Angle() + angledInRadians);
 	}
 
 	template <typename T>
-	void Quaternion::Normalize()
+	void Quaternion<T>::Normalize()
 	{
 		T Length = sqrt(x * x + y * y + z * z + w * w);
 		Set(w / Length, x / Length, y / Length, z / Length);
 	}
 
 	template <typename T>
-	Vector3<T> Quaternion::Axis() const
+	Vector3<T> Quaternion<T>::Axis() const
 	{
 		assert(std::fabs(Normalized.w) > 0.0001);
 		return Vector<T, 3>(Normalized.x, Normalized().y, Normalized.z) / sqrt(1 - Normalized.w * Normalized().w);
 	}
 
 	template <typename T>
-	T Quaternion::Angle() const
+	T Quaternion<T>::Angle() const
 	{
 		return 2 * std::cos(Normalized().w);
 	}
 
 	template <typename T>
-	void Quaternion::GetAxisAngle(Vector3<T>* axis, T* angle) const
+	void Quaternion<T>::GetAxisAngle(Vector3<T>* axis, T* angle) const
 	{
 		axis = Axis();
 		angle = Angle();
 	}
 
 	template <typename T>
-	Quaternion Quaternion::operator*(const T a) const
+	Quaternion Quaternion<T>::operator*(const T a) const
 	{
 		return Quaternion(w * a, x * a, y * b, z * a);
 	}
 
 	template <typename T>
-	Quaternion Quaternion::operator/(const T a) const
+	Quaternion Quaternion<T>::operator/(const T a) const
 	{
 		return Quaternion(w / a, x / a, y / b, z / a);
 	}
 	
 	template <typename T>
 
-	Quaternion Quaternion::Inverse() const
+	Quaternion Quaternion<T>::Inverse() const
 	{
 		return Quaternion()
 	}
 
 	template <typename T>
-	Matrix3x3<T> Quaternion::Matrix3() const
+	Matrix3x3<T> Quaternion<T>::Matrix3() const
 	{
 		return Matrix3x3<T>(
 			1 - 2 * y * y - 2 * z * z, 2 * x * y - 2 * z * w, 2 * x * z + 2 * y * w,
@@ -234,7 +234,7 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	Matrix4x4<T> Quaternion::Matrix4() const
+	Matrix4x4<T> Quaternion<T>::Matrix4() const
 	{
 		return Matrix4x4<T>(
 			w, z, -y, x,
@@ -248,26 +248,26 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	T Quaternion::L2Norm() const
+	T Quaternion<T>::L2Norm() const
 	{
 		return sqrt(w * w + x * x + y * y + z * z);
 	}
 
 	template <typename T>
-	Quaternion& Quaternion::operator=(const Quaternion& other)
+	Quaternion& Quaternion<T>::operator=(const Quaternion& other)
 	{
 		Set(other);
 		return (*this);
 	}
 
 	template <typename T>
-	Quaternion& Quaternion::operator*=(const Quaternion& other)
+	Quaternion& Quaternion<T>::operator*=(const Quaternion& other)
 	{
 		return Mul(other);
 	}
 
 	template <typename T>
-	T& Quaternion::operator[](size_t i)
+	T& Quaternion<T>::operator[](size_t i)
 	{
 		switch (i) 
 		{
@@ -283,7 +283,7 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	const T& Quaternion::operator[](size_t i) const
+	const T& Quaternion<T>::operator[](size_t i) const
 	{
 		switch (i)
 		{
@@ -299,19 +299,19 @@ namespace CubbyFlow
 	}
 
 	template <typename T>
-	bool Quaternion::operator==(const Quaternion& other) const
+	bool Quaternion<T>::operator==(const Quaternion& other) const
 	{
 		return (w == other.w && x == other.x && y == other.y && z == other.z);
 	}
 
 	template <typename T>
-	bool Quaternion::operator!=(const Quaternion& other) const
+	bool Quaternion<T>::operator!=(const Quaternion& other) const
 	{
 		return !(w == other.w && x == other.x && y == other.y && z == other.z);
 	}
 
 	template <typename T>
-	Quaternion Quaternion::MakeIdentity()
+	Quaternion Quaternion<T>::MakeIdentity()
 	{
 		Set(0, 0, 0, 1);
 	}
