ae514941c3a23ce9b32f602d4122a9dc22e99259
Chris Ohk <utilforever@gmail.com>
2017-03-29 01:14:57 +0900

    Upload BoundingBox2.h - 2-D box-ray intersection result

---
+++ Sources/BoundingBox/BoundingBox2.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: BoundingBox2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D box-ray intersection result.
+> Created Time: 2017/03/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_BOUNDING_BOX2_H
+#define CUBBYFLOW_BOUNDING_BOX2_H
+
+namespace CubbyFlow
+{
+
+}
+
+#endif
