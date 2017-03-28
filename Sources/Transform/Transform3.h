c8be6f581233cf42f0a19ce4a07b4543fecd1f23
Chris Ohk <utilforever@gmail.com>
2017-03-29 01:14:12 +0900

    Upload Transform3.h - Represents 3-D rigid body transform

---
+++ Sources/Transform/Transform3.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: Transform3.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Represents 3-D rigid body transform.
+> Created Time: 2017/03/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_TRANSFORM3_H
+#define CUBBYFLOW_TRANSFORM3_H
+
+namespace CubbyFlow
+{
+
+}
+
+#endif
