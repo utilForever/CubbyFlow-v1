6c6b8c124cead0e18040c2627835fd89713c3e00
Chris Ohk <utilforever@gmail.com>
2017-03-29 01:13:56 +0900

    Upload Transform2.h - Represents 2-D rigid body transform

---
+++ Sources/Transform/Transform2.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: Transform2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Represents 2-D rigid body transform.
+> Created Time: 2017/03/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_TRANSFORM2_H
+#define CUBBYFLOW_TRANSFORM2_H
+
+namespace CubbyFlow
+{
+
+}
+
+#endif
