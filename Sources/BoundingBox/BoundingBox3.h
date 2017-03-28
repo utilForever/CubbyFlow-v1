9c6709fc8ec2f95306416ef4ca7dacb28e6b11e5
Chris Ohk <utilforever@gmail.com>
2017-03-29 01:15:14 +0900

    Upload BoundingBox3.h - 3-D box-ray intersection result

---
+++ Sources/BoundingBox/BoundingBox3.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: BoundingBox3.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3-D box-ray intersection result.
+> Created Time: 2017/03/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_BOUNDING_BOX3_H
+#define CUBBYFLOW_BOUNDING_BOX3_H
+
+namespace CubbyFlow
+{
+
+}
+
+#endif
