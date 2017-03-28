681f0cdabcb0717a407862f6b6fe4a4109885851
Chris Ohk <utilforever@gmail.com>
2017-03-29 01:14:37 +0900

    Upload BoundingBox.h - Generic N-D axis-aligned bounding box class

---
+++ Sources/BoundingBox/BoundingBox.h
@@ -0,0 +1,17 @@
+/*************************************************************************
+> File Name: BoundingBox.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Generic N-D axis-aligned bounding box class.
+> Created Time: 2017/03/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_BOUNDING_BOX_H
+#define CUBBYFLOW_BOUNDING_BOX_H
+
+namespace CubbyFlow
+{
+
+}
+
+#endif
