d113e97824551dce886dded01665de6cf950788e
Chris Ohk <utilforever@gmail.com>
2017-04-01 11:30:04 +0900

    Update Ray2.cpp - Rafactor code, fix incorrect logic

--- Sources/Ray/Ray2.cpp
+++ Sources/Ray/Ray2.cpp
@@ -6,34 +6,34 @@
 > Created Time: 2017/03/25
 > Copyright (c) 2017, Dongmin Kim
 *************************************************************************/
-#include<Ray\Ray2.h>
+#include <Ray/Ray2.h>
 
 namespace CubbyFlow
 {
 	template <typename T>
-	Ray<T, 2>::Ray()
+	Ray<T, 2>::Ray() :
+		Ray(Vector2<T>(), Vector2<T>(1, 0))
 	{
-		origin(0, 0);
-		direction(1, 0);
+		// Do nothing
 	}
 
 	template <typename T>
-	Ray<T, 2>::Ray(const Vector2<T>& newOrigin, const Vector2<T>& newDirection)
+	Ray<T, 2>::Ray(const Vector2<T>& newOrigin, const Vector2<T>& newDirection) :
+		origin(newOrigin), direction(newDirection.Normalized())
 	{
-		origin(newOrigin);
-		direction(newDirection);
+		// Do nothing
 	}
 
 	template <typename T>
-	Ray<T, 2>::Ray(const Ray& other)
+	Ray<T, 2>::Ray(const Ray& other) :
+		origin(other.origin), direction(other.direction)
 	{
-		origin(other.origin);
-		direction(other.direction);
+		// Do nothing
 	}
 
 	template <typename T>
 	Vector2<T> Ray<T, 2>::PointAt(T t) const
 	{
-		return origin + (direction - origin).Normalized() * t;
+		return origin + t * direction;
 	}
 }
