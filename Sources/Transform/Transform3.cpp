f3fd01944e265ecaa0e7e28715ab620718b1acdc
Chris Ohk <utilforever@gmail.com>
2017-04-10 01:17:39 +0900

    Update Transform3.cpp - Fix compile error and refactor code

--- Sources/Transform/Transform3.cpp
+++ Sources/Transform/Transform3.cpp
@@ -6,7 +6,7 @@
 > Created Time: 2017/03/30
 > Copyright (c) 2017, Dongmin Kim
 *************************************************************************/
-#include<Transform\Transform3.h>
+#include <Transform/Transform3.h>
 
 namespace CubbyFlow
 {
@@ -18,8 +18,8 @@ namespace CubbyFlow
 
 	Transform3::Transform3(const Vector3D& translation, const QuaternionD& orientation)
 	{
-		m_translation = translation;
-		m_orientation = orientation;
+		SetTranslation(translation);
+		SetOrientation(orientation);
 	}
 
 	const Vector3D& Transform3::Translation() const
@@ -32,47 +32,78 @@ namespace CubbyFlow
 		m_translation = translation;
 	}
 
+	const QuaternionD& Transform3::Orientation() const
+	{
+		return m_orientation;
+	}
+
 	void Transform3::SetOrientation(const QuaternionD& orientation)
 	{
 		m_orientation = orientation;
+		m_orientationMat3 = orientation.Matrix3();
+		m_inverseOrientationMat3 = orientation.Inverse().Matrix3();
 	}
 
 	Vector3D Transform3::ToLocal(const Vector3D& pointInWorld) const
 	{
-		return m_orientation.Inverse().Mul(m_orientation * m_orientation.Mul(pointInWorld - m_translation));
+		return m_inverseOrientationMat3 * (pointInWorld - m_translation);
 	}
 
 	Vector3D Transform3::ToLocalDirection(const Vector3D& dirInWorld) const
 	{
-		return m_orientation.Inverse().Mul(m_orientation * dirInWorld);
+		return m_inverseOrientationMat3 * dirInWorld;
 	}
 
 	Ray3D Transform3::ToLocal(const Ray3D& rayInWorld) const
 	{
-		return Ray3D(ToLocal(rayInWorld.origin), ToLocal(rayInWorld.direction));
+		return Ray3D(
+			ToLocal(rayInWorld.origin),
+			ToLocalDirection(rayInWorld.direction));
 	}
 
 	BoundingBox3D Transform3::ToLocal(const BoundingBox3D& bboxInWorld) const
 	{
+		BoundingBox3D bboxInLocal = bboxInWorld;
+		
+		for (size_t i = 0; i < 8; ++i)
+		{
+			auto cornerInLocal = ToLocal(bboxInWorld.Corner(i));
+
+			bboxInLocal.lowerCorner	= std::min(bboxInLocal.lowerCorner, cornerInLocal);
+			bboxInLocal.upperCorner = std::max(bboxInLocal.upperCorner, cornerInLocal);
+		}
+
+		return bboxInLocal;
 	}
 
 	Vector3D Transform3::ToWorld(const Vector3D& pointInLocal) const
 	{
-		return m_orientation.Inverse().Mul(m_orientation*(pointInLocal + m_translation));
+		return (m_orientationMat3 * pointInLocal) + m_translation;
 	}
 
 	Vector3D Transform3::ToWorldDirection(const Vector3D& dirInLocal) const
 	{
-		return m_orientation.Inverse().Mul(m_orientation*(dirInLocal));
+		return m_orientationMat3 * dirInLocal;
 	}
 
 	Ray3D Transform3::ToWorld(const Ray3D& rayInLocal) const
 	{
-		return Ray3D(ToWorld(rayInLocal.origin), ToWorld(rayInLocal.direction));
+		return Ray3D(
+			ToWorld(rayInLocal.origin),
+			ToWorldDirection(rayInLocal.direction));
 	}
 
 	BoundingBox3D Transform3::ToWorld(const BoundingBox3D& bboxInLocal) const
 	{
+		BoundingBox3D bboxInWorld = bboxInLocal;
 
+		for (size_t i = 0; i < 8; ++i)
+		{
+			auto cornerInWorld = ToWorld(bboxInLocal.Corner(i));
+			bboxInWorld.lowerCorner	= std::min(bboxInWorld.lowerCorner, cornerInWorld);
+			bboxInWorld.upperCorner	= std::max(bboxInWorld.upperCorner, cornerInWorld);
+		}
+
+		return bboxInWorld;
 	}
 }
