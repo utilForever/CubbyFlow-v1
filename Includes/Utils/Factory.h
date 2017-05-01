9f7ac285bd86b98844f00d135e8ee5eef455bf39
Chris Ohk <utilforever@gmail.com>
2017-05-01 22:37:47 +0900

    Upload Factory.h - Factory methods for CubbyFlow

---
+++ Includes/Utils/Factory.h
@@ -0,0 +1,22 @@
+/*************************************************************************
+> File Name: Factory.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: Factory methods for CubbyFlow.
+> Created Time: 2017/05/01
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_FACTORY_H
+#define CUBBYFLOW_FACTORY_H
+
+#include <Searcher/PointNeighborSearcher2.h>
+
+namespace CubbyFlow
+{
+	class Factory
+	{
+		static PointNeighborSearcher2Ptr BuildPointNeighborSearcher2(const std::string& name);
+	};
+}
+
+#endif
