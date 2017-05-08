edb53028a2f39e78c7258feeec682908d6bb421e
Chris Ohk <utilforever@gmail.com>
2017-05-08 13:04:58 +0900

    Upload ParticleSystemData3.h - 3-D particle system data

---
+++ Includes/Particle/ParticleSystemData3.h
@@ -0,0 +1,35 @@
+/*************************************************************************
+> File Name: ParticleSystemData3.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3-D particle system data.
+> Created Time: 2017/05/08
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_PARTICLE_SYSTEM_DATA3_H
+#define CUBBYFLOW_PARTICLE_SYSTEM_DATA3_H
+
+#ifndef CUBBYFLOW_DOXYGEN
+
+namespace flatbuffers
+{
+	class FlatBufferBuilder;
+	template <typename T> struct Offset;
+}
+
+namespace CubbyFlow
+{
+	namespace fbs
+	{
+		struct ParticleSystemData3;
+	}
+}
+
+#endif
+
+namespace CubbyFlow
+{
+		
+}
+
+#endif
