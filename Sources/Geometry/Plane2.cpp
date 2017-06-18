de0e9c046c325fab507c881944af4ded66c1c2bd
Chris Ohk <utilforever@gmail.com>
2017-06-18 23:15:02 +0900

    Upload Plane2.cpp - 2-D plane geometry

---
+++ Sources/Geometry/Plane2.cpp
@@ -0,0 +1,120 @@
+/*************************************************************************
+> File Name: Plane2.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D plane geometry.
+> Created Time: 2017/06/18
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <Geometry/Plane2.h>
+
+namespace CubbyFlow
+{
+	Plane2::Plane2(const Transform2& transform_, bool isNormalFlipped_) :
+		Surface2(transform_, isNormalFlipped_)
+	{
+		// Do nothing
+	}
+
+	Plane2::Plane2(const Vector2D& normal_, const Vector2D& point_,
+		const Transform2& transform_, bool isNormalFlipped_) :
+		Surface2(transform_, isNormalFlipped_), normal(normal_), point(point_)
+	{
+		// Do nothing
+	}
+
+	Plane2::Plane2(const Plane2& other) :
+		Surface2(other), normal(other.normal), point(other.point)
+	{
+		// Do nothing
+	}
+
+	Vector2D Plane2::ClosestPointLocal(const Vector2D& otherPoint) const
+	{
+		Vector2D r = otherPoint - point;
+		return r - normal.Dot(r) * normal + point;
+	}
+
+	Vector2D Plane2::ClosestNormalLocal([[maybe_unused]] const Vector2D& otherPoint) const
+	{
+		return normal;
+	}
+
+	bool Plane2::IntersectsLocal(const Ray2D& ray) const
+	{
+		return std::fabs(ray.direction.Dot(normal)) > 0;
+	}
+
+	SurfaceRayIntersection2 Plane2::ClosestIntersectionLocal(const Ray2D& ray) const
+	{
+		SurfaceRayIntersection2 intersection;
+		double dDotN = ray.direction.Dot(normal);
+
+		// Check if not parallel
+		if (std::fabs(dDotN) > 0)
+		{
+			double t = normal.Dot(point - ray.origin) / dDotN;
+			if (t >= 0.0)
+			{
+				intersection.isIntersecting = true;
+				intersection.t = t;
+				intersection.point = ray.PointAt(t);
+				intersection.normal = normal;
+			}
+		}
+
+		return intersection;
+	}
+
+	BoundingBox2D Plane2::BoundingBoxLocal() const
+	{
+		if (std::fabs(normal.Dot(Vector2D(1, 0)) - 1.0) < std::numeric_limits<double>::epsilon())
+		{
+			return BoundingBox2D(
+				point - Vector2D(0, std::numeric_limits<double>::max()),
+				point + Vector2D(0, std::numeric_limits<double>::max()));
+		}
+		
+		if (std::fabs(normal.Dot(Vector2D(0, 1)) - 1.0) < std::numeric_limits<double>::epsilon())
+		{
+			return BoundingBox2D(
+				point - Vector2D(std::numeric_limits<double>::max(), 0),
+				point + Vector2D(std::numeric_limits<double>::max(), 0));
+		}
+		
+		return BoundingBox2D(
+			Vector2D(std::numeric_limits<double>::max(), std::numeric_limits<double>::max()),
+			Vector2D(std::numeric_limits<double>::max(), std::numeric_limits<double>::max()));
+	}
+
+	Plane2::Builder Plane2::GetBuilder()
+	{
+		return Builder();
+	}
+
+	Plane2::Builder& Plane2::Builder::WithNormal(const Vector2D& normal)
+	{
+		m_normal = normal;
+		return *this;
+	}
+
+	Plane2::Builder& Plane2::Builder::WithPoint(const Vector2D& point)
+	{
+		m_point = point;
+		return *this;
+	}
+
+	Plane2 Plane2::Builder::Build() const
+	{
+		return Plane2(m_normal, m_point, m_transform, m_isNormalFlipped);
+	}
+
+	Plane2Ptr Plane2::Builder::MakeShared() const
+	{
+		return std::shared_ptr<Plane2>(new Plane2(m_normal, m_point, m_transform, m_isNormalFlipped),
+			[](Plane2* obj)
+		{
+			delete obj;
+		});
+	}
+}
