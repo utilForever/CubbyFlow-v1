3270e2e9c71a8f195e850d7948d8698afa93b1ef
Chris Ohk <utilforever@gmail.com>
2017-07-16 18:32:32 +0900

    Update Constants.h - Add constants, functions for PI/2, PI/4

--- Includes/Utils/Constants.h
+++ Includes/Utils/Constants.h
@@ -95,6 +95,62 @@ namespace CubbyFlow
 		return PI_DOUBLE;
 	}
 
+	// MARK: PI/2
+	//! Float-type PI/2.
+	constexpr float HALF_PI_FLOAT = 1.57079632679489661923132169163975144f;
+
+	//! Double-type PI/2.
+	constexpr double HALF_PI_DOUBLE = 1.57079632679489661923132169163975144;
+
+	//! PI/2 for type T.
+	template <typename T>
+	constexpr T HalfPI()
+	{
+		return static_cast<T>(HALF_PI_DOUBLE);
+	}
+
+	//! PI/2 for float.
+	template <>
+	constexpr float HalfPI<float>()
+	{
+		return HALF_PI_FLOAT;
+	}
+
+	//! PI/2 for double.
+	template <>
+	constexpr double HalfPI<double>()
+	{
+		return HALF_PI_DOUBLE;
+	}
+
+	// MARK: PI/4
+	//! Float-type PI/4.
+	constexpr float QUARTER_PI_FLOAT = 0.785398163397448309615660845819875721f;
+
+	//! Double-type PI/4.
+	constexpr double QUARTER_PI_DOUBLE = 0.785398163397448309615660845819875721;
+
+	//! PI/4 for type T.
+	template <typename T>
+	constexpr T QuarterPI()
+	{
+		return static_cast<T>(QUARTER_PI_DOUBLE);
+	}
+
+	//! PI/4 for float.
+	template <>
+	constexpr float QuarterPI<float>()
+	{
+		return QUARTER_PI_FLOAT;
+	}
+
+	//! PI/4 for double.
+	template <>
+	constexpr double QuarterPI<double>()
+	{
+		return QUARTER_PI_DOUBLE;
+	}
+
 	// MARK: Physics
 	//! Gravity.
 	constexpr double GRAVITY = -9.8;
