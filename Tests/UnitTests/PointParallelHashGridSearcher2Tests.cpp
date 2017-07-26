01fabac15224affbaf0e41258c225c810f5da509
김동민 <dmk98@naver.com>
2017-07-26 14:08:53 +0900

    Update PointParallelHashGridSearcher Test
    Temporary

--- Tests/UnitTests/PointParallelHashGridSearcher2Tests.cpp
+++ Tests/UnitTests/PointParallelHashGridSearcher2Tests.cpp
@@ -32,12 +32,54 @@ TEST(PointParallelHashGridSearcher2, ForEachNearByPoint)
 	});
 }
 
-TEST(PointParallelHashGridSearcher2, ForEachNearByPointEmpty)
+TEST(PointParallelHashGridSearcher2, HasEachNearByPoint)
 {
-	Array1<Vector2D> points;
+	Array1<Vector2D> points = {
+		Vector2D(1, 1),
+		Vector2D(3, 4),
+		Vector2D(4, 5)
+	};
 
 	PointParallelHashGridSearcher2 searcher(4, 4, std::sqrt(10));
 	searcher.Build(points.Accessor());
 
-	searcher.ForEachNearbyPoint()
+	bool result;
+	result = searcher.HasNearbyPoint(Vector2D(), std::sqrt(15.0));
+
+	EXPECT_TRUE(result);
+}
+
+TEST(PointParallelHashGridSearcher2, Build)
+{
+	Array1<Vector2D> points = {
+		Vector2D(3, 4),
+		Vector2D(1, 5),
+		Vector2D(-3, 0)
+	};
+
+	PointParallelHashGridSearcher2 searcher(4, 4, std::sqrt(10));
+	searcher.Build(points.Accessor());
+
+	EXPECT_EQ(4, searcher.GetHashKeyFromBucketIndex(Point2I(0, 1)));
+	EXPECT_EQ(8, searcher.GetHashKeyFromBucketIndex(Point2I(0, 2)));
+	EXPECT_EQ(3, searcher.GetHashKeyFromBucketIndex(Point2I(-1, 0)));
+	
+	std::vector<size_t> resultKey;
+	
+	resultKey.push_back(3);
+	resultKey.push_back(4);
+	resultKey.push_back(8);
+
+	for (size_t i = 0; i < 3; i++)
+	{
+		EXPECT_EQ(resultKey[i], searcher.Keys[i]);
+	}
+
+	EXPECT_EQ(0, searcher.StartIndexTable[3]);
+	EXPECT_EQ(1, searcher.StartIndexTable[4]);
+	EXPECT_EQ(2, searcher.StartIndexTable[8]);
+
+	EXPECT_EQ(1, searcher.EndIndexTable[3]);
+	EXPECT_EQ(2, searcher.EndIndexTable[4]);
+	EXPECT_EQ(3, searcher.EndIndexTable[8]);
 }
--- Tests/UnitTests/PointSimpleListSeracher2Tests.cpp
+++ Tests/UnitTests/PointSimpleListSeracher2Tests.cpp
@@ -32,3 +32,57 @@ TEST(PointSimpleListSearcher2, ForEachNearByPoint)
 	});
 }
 
+TEST(PointSimpleListSearcher2, HasNearByPoint)
+{
+	Array1<Vector2D> points = {
+		Vector2D(1, 3),
+		Vector2D(3, 2),
+		Vector2D(-1, 2)
+	};
+
+	PointSimpleListSearcher2 searcher;
+	searcher.Build(points.Accessor());
+
+	bool result;
+	result = searcher.HasNearbyPoint(Vector2D(0, 0), std::sqrt(10.0));
+
+	EXPECT_TRUE(result);
+}
+
+TEST(PointSimpleListSearcher2, Serialize)
+{
+	Array1<Vector2D> points = {
+		Vector2D(1, 3),
+		Vector2D(2, 4),
+		Vector2D(3, 1)
+	};
+
+	std::vector<const Vector2D&> result;
+
+	PointSimpleListSearcher2 searcher;
+	searcher.Build(points.Accessor());
+
+	std::vector<uint8_t>* buf;
+
+	searcher.Serialize(buf);
+
+	PointSimpleListSearcher2 searcher2;
+	searcher2.Deserialize(*buf);
+
+	searcher.ForEachNearbyPoint(
+		Vector2D(0, 0),
+		std::sqrt(std::numeric_limits<double>::max()),
+		[&](size_t i, const Vector2D& pt)
+	{
+		result.push_back(pt);
+	});
+
+	searcher2.ForEachNearbyPoint(
+		Vector2D(0, 0),
+		std::sqrt(std::numeric_limits<double>::max()),
+		[&](size_t i, const Vector2D& pt)
+	{
+		EXPECT_EQ(result[i].x, pt.x);
+		EXPECT_EQ(result[i].y, pt.y);
+	});
+}
