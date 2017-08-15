ab50d9dbdb5041691e6b3353877cc0ae47f9b045
Chris Ohk <utilforever@gmail.com>
2017-08-15 23:27:31 +0900

    Update GridFluidSolver2.h - Refactor code

--- Includes/Solver/Grid/GridFluidSolver2.h
+++ Includes/Solver/Grid/GridFluidSolver2.h
@@ -48,13 +48,13 @@ namespace CubbyFlow
 		virtual ~GridFluidSolver2();
 
 		//! Returns the gravity vector of the system.
-		const Vector2D& gravity() const;
+		const Vector2D& GetGravity() const;
 
 		//! Sets the gravity of the system.
-		void setGravity(const Vector2D& newGravity);
+		void SetGravity(const Vector2D& newGravity);
 
 		//! Returns the viscosity coefficient.
-		double viscosityCoefficient() const;
+		double GetViscosityCoefficient() const;
 
 		//!
 		//! \brief Sets the viscosity coefficient.
@@ -64,45 +64,44 @@ namespace CubbyFlow
 		//!
 		//! \param[in] newValue The new viscosity coefficient value.
 		//!
-		void setViscosityCoefficient(double newValue);
+		void SetViscosityCoefficient(double newValue);
 
 		//!
-		//! \brief Returns the CFL number from the current velocity field for given
-		//!     time interval.
+		//! \brief Returns the CFL number from the current velocity field for given time interval.
 		//!
 		//! \param[in] timeIntervalInSeconds The time interval in seconds.
 		//!
-		double cfl(double timeIntervalInSeconds) const;
+		double GetCFL(double timeIntervalInSeconds) const;
 
 		//! Returns the max allowed CFL number.
-		double maxCfl() const;
+		double GetMaxCFL() const;
 
 		//! Sets the max allowed CFL number.
-		void setMaxCfl(double newCfl);
+		void SetMaxCFL(double newCFL);
 
 		//! Returns the advection solver instance.
-		const AdvectionSolver2Ptr& advectionSolver() const;
+		const AdvectionSolver2Ptr& GetAdvectionSolver() const;
 
 		//! Sets the advection solver.
-		void setAdvectionSolver(const AdvectionSolver2Ptr& newSolver);
+		void SetAdvectionSolver(const AdvectionSolver2Ptr& newSolver);
 
 		//! Returns the diffusion solver instance.
-		const GridDiffusionSolver2Ptr& diffusionSolver() const;
+		const GridDiffusionSolver2Ptr& GetDiffusionSolver() const;
 
 		//! Sets the diffusion solver.
-		void setDiffusionSolver(const GridDiffusionSolver2Ptr& newSolver);
+		void SetDiffusionSolver(const GridDiffusionSolver2Ptr& newSolver);
 
 		//! Returns the pressure solver instance.
-		const GridPressureSolver2Ptr& pressureSolver() const;
+		const GridPressureSolver2Ptr& GetPressureSolver() const;
 
 		//! Sets the pressure solver.
-		void setPressureSolver(const GridPressureSolver2Ptr& newSolver);
+		void SetPressureSolver(const GridPressureSolver2Ptr& newSolver);
 
 		//! Returns the closed domain boundary flag.
-		int closedDomainBoundaryFlag() const;
+		int GetClosedDomainBoundaryFlag() const;
 
 		//! Sets the closed domain boundary flag.
-		void setClosedDomainBoundaryFlag(int flag);
+		void SetClosedDomainBoundaryFlag(int flag);
 
 		//!
 		//! \brief Returns the grid system data.
@@ -113,10 +112,10 @@ namespace CubbyFlow
 		//!
 		//! \see GridSystemData2
 		//!
-		const GridSystemData2Ptr& gridSystemData() const;
+		const GridSystemData2Ptr& GetGridSystemData() const;
 
 		//!
-		//! \brief	s grid system data.
+		//! \brief Resizes grid system data.
 		//!
 		//! This function resizes grid system data. You can also resize the grid by
 		//! calling resize function directly from
@@ -127,7 +126,7 @@ namespace CubbyFlow
 		//! \param[in] newGridSpacing The new grid spacing.
 		//! \param[in] newGridOrigin  The new grid origin.
 		//!
-		void resizeGrid(
+		void ResizeGrid(
 			const Size2& newSize,
 			const Vector2D& newGridSpacing,
 			const Vector2D& newGridOrigin);
@@ -139,7 +138,7 @@ namespace CubbyFlow
 		//! equivalent to calling gridSystemData()->resolution(), but provides a
 		//! shortcut.
 		//!
-		Size2 resolution() const;
+		Size2 GetGridResolution() const;
 
 		//!
 		//! \brief Returns the grid spacing of the grid system data.
@@ -148,7 +147,7 @@ namespace CubbyFlow
 		//! equivalent to calling gridSystemData()->gridSpacing(), but provides a
 		//! shortcut.
 		//!
-		Vector2D gridSpacing() const;
+		Vector2D GetGridSpacing() const;
 
 		//!
 		//! \brief Returns the origin of the grid system data.
@@ -157,7 +156,7 @@ namespace CubbyFlow
 		//! equivalent to calling gridSystemData()->origin(), but provides a
 		//! shortcut.
 		//!
-		Vector2D gridOrigin() const;
+		Vector2D GetGridOrigin() const;
 
 		//!
 		//! \brief Returns the velocity field.
@@ -165,22 +164,22 @@ namespace CubbyFlow
 		//! This function returns the velocity field from the grid system data.
 		//! It is just a shortcut to the most commonly accessed data chunk.
 		//!
-		const FaceCenteredGrid2Ptr& velocity() const;
+		const FaceCenteredGrid2Ptr& GetVelocity() const;
 
 		//! Returns the collider.
-		const Collider2Ptr& collider() const;
+		const Collider2Ptr& GetCollider() const;
 
 		//! Sets the collider.
-		void setCollider(const Collider2Ptr& newCollider);
+		void SetCollider(const Collider2Ptr& newCollider);
 
 		//! Returns the emitter.
-		const GridEmitter2Ptr& emitter() const;
+		const GridEmitter2Ptr& GetEmitter() const;
 
 		//! Sets the emitter.
-		void setEmitter(const GridEmitter2Ptr& newEmitter);
+		void SetEmitter(const GridEmitter2Ptr& newEmitter);
 
 		//! Returns builder fox GridFluidSolver2.
-		static Builder builder();
+		static Builder GetBuilder();
 
 	protected:
 		//! Called when it needs to setup initial condition.
@@ -197,16 +196,15 @@ namespace CubbyFlow
 		//! too large so that it makes the CFL number greater than the max value,
 		//! This function will return a number that is greater than 1.
 		//!
-		//! \see GridFluidSolver2::maxCfl
+		//! \see GridFluidSolver2::GetMaxCFL
 		//!
-		unsigned int NumberOfSubTimeSteps(
-			double timeIntervalInSeconds) const override;
+		unsigned int NumberOfSubTimeSteps(double timeIntervalInSeconds) const override;
 
 		//! Called at the beginning of a time-step.
-		virtual void onBeginAdvanceTimeStep(double timeIntervalInSeconds);
+		virtual void OnBeginAdvanceTimeStep(double timeIntervalInSeconds);
 
 		//! Called at the end of a time-step.
-		virtual void onEndAdvanceTimeStep(double timeIntervalInSeconds);
+		virtual void OnEndAdvanceTimeStep(double timeIntervalInSeconds);
 
 		//!
 		//! \brief Computes the external force terms.
@@ -214,31 +212,32 @@ namespace CubbyFlow
 		//! This function computes the external force applied for given time
 		//! interval. By default, it only computes the gravity.
 		//!
-		//! \see GridFluidSolver2::computeGravity
+		//! \see GridFluidSolver2::ComputeGravity
 		//!
-		virtual void computeExternalForces(double timeIntervalInSeconds);
+		virtual void ComputeExternalForces(double timeIntervalInSeconds);
 
 		//! Computes the viscosity term using the diffusion solver.
-		virtual void computeViscosity(double timeIntervalInSeconds);
+		virtual void ComputeViscosity(double timeIntervalInSeconds);
 
 		//! Computes the pressure term using the pressure solver.
-		virtual void computePressure(double timeIntervalInSeconds);
+		virtual void ComputePressure(double timeIntervalInSeconds);
 
 		//! Computes the advection term using the advection solver.
-		virtual void computeAdvection(double timeIntervalInSeconds);
+		virtual void ComputeAdvection(double timeIntervalInSeconds);
 
 		//!
-		//! \breif Returns the signed-distance representation of the fluid.
+		//! \brief Returns the signed-distance representation of the fluid.
 		//!
 		//! This function returns the signed-distance representation of the fluid.
 		//! Positive sign area is considered to be atmosphere and won't be included
 		//! for computing the dynamics. By default, this will return constant scalar
-		//! field of -kMaxD, meaning that the entire volume is occupied with fluid.
+		//! field of -std::numeric_limits<double>::max(), meaning that
+		//! the entire volume is occupied with fluid.
 		//!
-		virtual ScalarField2Ptr fluidSdf() const;
+		virtual ScalarField2Ptr GetFluidSDF() const;
 
 		//! Computes the gravity term.
-		void computeGravity(double timeIntervalInSeconds);
+		void ComputeGravity(double timeIntervalInSeconds);
 
 		//!
 		//! \brief Applies the boundary condition to the velocity field.
@@ -246,65 +245,65 @@ namespace CubbyFlow
 		//! This function applies the boundary condition to the velocity field by
 		//! constraining the flow based on the boundary condition solver.
 		//!
-		void applyBoundaryCondition();
+		void ApplyBoundaryCondition();
 
 		//! Extrapolates given field into the collider-occupied region.
-		void extrapolateIntoCollider(ScalarGrid2* grid);
+		void ExtrapolateIntoCollider(ScalarGrid2* grid);
 
 		//! Extrapolates given field into the collider-occupied region.
-		void extrapolateIntoCollider(CollocatedVectorGrid2* grid);
+		void ExtrapolateIntoCollider(CollocatedVectorGrid2* grid);
 
 		//! Extrapolates given field into the collider-occupied region.
-		void extrapolateIntoCollider(FaceCenteredGrid2* grid);
+		void ExtrapolateIntoCollider(FaceCenteredGrid2* grid);
 
 		//! Returns the signed-distance field representation of the collider.
-		ScalarField2Ptr colliderSdf() const;
+		ScalarField2Ptr GetColliderSDF() const;
 
 		//! Returns the velocity field of the collider.
-		VectorField2Ptr colliderVelocityField() const;
+		VectorField2Ptr GetColliderVelocityField() const;
 
 	private:
-		Vector2D _gravity = Vector2D(0.0, -9.8);
-		double _viscosityCoefficient = 0.0;
-		double _maxCfl = 5.0;
-		int _closedDomainBoundaryFlag = kDirectionAll;
+		Vector2D m_gravity = Vector2D(0.0, -9.8);
+		double m_viscosityCoefficient = 0.0;
+		double m_maxCFL = 5.0;
+		int m_closedDomainBoundaryFlag = DIRECTION_ALL;
 
-		GridSystemData2Ptr _grids;
-		Collider2Ptr _collider;
-		GridEmitter2Ptr _emitter;
+		GridSystemData2Ptr m_grids;
+		Collider2Ptr m_collider;
+		GridEmitter2Ptr m_emitter;
 
-		AdvectionSolver2Ptr _advectionSolver;
-		GridDiffusionSolver2Ptr _diffusionSolver;
-		GridPressureSolver2Ptr _pressureSolver;
-		GridBoundaryConditionSolver2Ptr _boundaryConditionSolver;
+		AdvectionSolver2Ptr m_advectionSolver;
+		GridDiffusionSolver2Ptr m_diffusionSolver;
+		GridPressureSolver2Ptr m_pressureSolver;
+		GridBoundaryConditionSolver2Ptr m_boundaryConditionSolver;
 
-		void beginAdvanceTimeStep(double timeIntervalInSeconds);
+		void BeginAdvanceTimeStep(double timeIntervalInSeconds);
 
-		void endAdvanceTimeStep(double timeIntervalInSeconds);
+		void EndAdvanceTimeStep(double timeIntervalInSeconds);
 
-		void updateCollider(double timeIntervalInSeconds);
+		void UpdateCollider(double timeIntervalInSeconds);
 
-		void updateEmitter(double timeIntervalInSeconds);
+		void UpdateEmitter(double timeIntervalInSeconds);
 	};
 
 	//! Shared pointer type for the GridFluidSolver2.
-	typedef std::shared_ptr<GridFluidSolver2> GridFluidSolver2Ptr;
-
-
+	using GridFluidSolver2Ptr = std::shared_ptr<GridFluidSolver2>;
+	
 	//!
 	//! \brief Base class for grid-based fluid solver builder.
 	//!
 	template <typename DerivedBuilder>
-	class GridFluidSolverBuilderBase2 {
+	class GridFluidSolverBuilderBase2
+	{
 	public:
 		//! Returns builder with grid resolution.
-		DerivedBuilder& withResolution(const Size2& resolution);
+		DerivedBuilder& WithResolution(const Size2& resolution);
 
 		//! Returns builder with grid spacing.
-		DerivedBuilder& withGridSpacing(const Vector2D& gridSpacing);
+		DerivedBuilder& WithGridSpacing(const Vector2D& gridSpacing);
 
 		//! Returns builder with grid spacing.
-		DerivedBuilder& withGridSpacing(double gridSpacing);
+		DerivedBuilder& WithGridSpacing(double gridSpacing);
 
 		//!
 		//! \brief Returns builder with domain size in x-direction.
@@ -312,80 +311,86 @@ namespace CubbyFlow
 		//! To build a solver, one can use either grid spacing directly or domain
 		//! size in x-direction to set the final grid spacing.
 		//!
-		DerivedBuilder& withDomainSizeX(double domainSizeX);
+		DerivedBuilder& WithDomainSizeX(double domainSizeX);
 
 		//! Returns builder with grid origin
-		DerivedBuilder& withOrigin(const Vector2D& gridOrigin);
+		DerivedBuilder& WithOrigin(const Vector2D& gridOrigin);
 
 	protected:
-		Size2 _resolution{ 1, 1 };
-		Vector2D _gridSpacing{ 1, 1 };
-		Vector2D _gridOrigin{ 0, 0 };
-		double _domainSizeX = 1.0;
-		bool _useDomainSize = false;
+		Size2 m_resolution{ 1, 1 };
+		Vector2D m_gridSpacing{ 1, 1 };
+		Vector2D m_gridOrigin{ 0, 0 };
+		double m_domainSizeX = 1.0;
+		bool m_useDomainSize = false;
 
-		Vector2D getGridSpacing() const;
+		Vector2D GetGridSpacing() const;
 	};
 
 	template <typename T>
-	T& GridFluidSolverBuilderBase2<T>::withResolution(const Size2& resolution) {
-		_resolution = resolution;
+	T& GridFluidSolverBuilderBase2<T>::WithResolution(const Size2& resolution)
+	{
+		m_resolution = resolution;
 		return static_cast<T&>(*this);
 	}
 
 	template <typename T>
-	T& GridFluidSolverBuilderBase2<T>::withGridSpacing(
-		const Vector2D& gridSpacing) {
-		_gridSpacing = gridSpacing;
-		_useDomainSize = false;
+	T& GridFluidSolverBuilderBase2<T>::WithGridSpacing(const Vector2D& gridSpacing)
+	{
+		m_gridSpacing = gridSpacing;
+		m_useDomainSize = false;
 		return static_cast<T&>(*this);
 	}
 
 	template <typename T>
-	T& GridFluidSolverBuilderBase2<T>::withGridSpacing(double gridSpacing) {
-		_gridSpacing.x = gridSpacing;
-		_gridSpacing.y = gridSpacing;
-		_useDomainSize = false;
+	T& GridFluidSolverBuilderBase2<T>::WithGridSpacing(double gridSpacing)
+	{
+		m_gridSpacing.x = gridSpacing;
+		m_gridSpacing.y = gridSpacing;
+		m_useDomainSize = false;
 		return static_cast<T&>(*this);
 	}
 
 	template <typename T>
-	T& GridFluidSolverBuilderBase2<T>::withDomainSizeX(double domainSizeX) {
-		_domainSizeX = domainSizeX;
-		_useDomainSize = true;
+	T& GridFluidSolverBuilderBase2<T>::WithDomainSizeX(double domainSizeX)
+	{
+		m_domainSizeX = domainSizeX;
+		m_useDomainSize = true;
 		return static_cast<T&>(*this);
 	}
 
 	template <typename T>
-	T& GridFluidSolverBuilderBase2<T>::withOrigin(const Vector2D& gridOrigin) {
-		_gridOrigin = gridOrigin;
+	T& GridFluidSolverBuilderBase2<T>::WithOrigin(const Vector2D& gridOrigin)
+	{
+		m_gridOrigin = gridOrigin;
 		return static_cast<T&>(*this);
 	}
 
 	template <typename T>
-	Vector2D GridFluidSolverBuilderBase2<T>::getGridSpacing() const {
-		Vector2D gridSpacing = _gridSpacing;
-		if (_useDomainSize) {
-			gridSpacing.set(_domainSizeX / static_cast<double>(_resolution.x));
+	Vector2D GridFluidSolverBuilderBase2<T>::GetGridSpacing() const
+	{
+		Vector2D gridSpacing = m_gridSpacing;
+		
+		if (m_useDomainSize)
+		{
+			gridSpacing.set(m_domainSizeX / static_cast<double>(m_resolution.x));
 		}
+
 		return gridSpacing;
 	}
 
 	//!
 	//! \brief Front-end to create GridFluidSolver2 objects step by step.
 	//!
-	class GridFluidSolver2::Builder final
-		: public GridFluidSolverBuilderBase2<GridFluidSolver2::Builder>{
+	class GridFluidSolver2::Builder final : public GridFluidSolverBuilderBase2<GridFluidSolver2::Builder>
+	{
 	public:
 		//! Builds GridFluidSolver2.
-		GridFluidSolver2 build() const;
+		GridFluidSolver2 Build() const;
 
 		//! Builds shared pointer of GridFluidSolver2 instance.
-		GridFluidSolver2Ptr makeShared() const {
-			return std::make_shared<GridFluidSolver2>(
-				_resolution,
-				getGridSpacing(),
-				_gridOrigin);
+		GridFluidSolver2Ptr MakeShared() const
+		{
+			return std::make_shared<GridFluidSolver2>(m_resolution, GetGridSpacing(), m_gridOrigin);
 		}
 	};
 }
