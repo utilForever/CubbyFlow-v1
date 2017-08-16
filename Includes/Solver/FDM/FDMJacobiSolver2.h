0fc07014605bac778d8acee0423e70faf5278bf2
Chris Ohk <utilforever@gmail.com>
2017-08-17 00:57:49 +0900

    Upload FDMJacobiSolver2.h - 2-D finite difference-type linear system solver using Jacobi method

---
+++ Includes/Solver/FDM/FDMJacobiSolver2.h
@@ -0,0 +1,58 @@
+/*************************************************************************
+> File Name: FDMJacobiSolver2.h
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: 2-D finite difference-type linear system solver using Jacobi method.
+> Created Time: 2017/08/17
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef CUBBYFLOW_FDM_CG_SOLVER3_H
+#define CUBBYFLOW_FDM_CG_SOLVER3_H
+
+#include <Solver/FDM/FDMLinearSystemSolver2.h>
+
+namespace CubbyFlow
+{
+	//! \brief 2-D finite difference-type linear system solver using Jacobi method.
+	class FDMJacobiSolver2 final : public FDMLinearSystemSolver2
+	{
+	public:
+		//! Constructs the solver with given parameters.
+		FDMJacobiSolver2(
+			unsigned int maxNumberOfIterations,
+			unsigned int residualCheckInterval,
+			double tolerance);
+
+		//! Solves the given linear system.
+		bool Solve(FDMLinearSystem2* system) override;
+
+		//! Returns the max number of Jacobi iterations.
+		unsigned int GetMaxNumberOfIterations() const;
+
+		//! Returns the last number of Jacobi iterations the solver made.
+		unsigned int GetLastNumberOfIterations() const;
+
+		//! Returns the max residual tolerance for the Jacobi method.
+		double GetTolerance() const;
+
+		//! Returns the last residual after the Jacobi iterations.
+		double GetLastResidual() const;
+
+	private:
+		unsigned int m_maxNumberOfIterations;
+		unsigned int m_lastNumberOfIterations;
+		unsigned int m_residualCheckInterval;
+		double m_tolerance;
+		double m_lastResidual;
+
+		FDMVector2 m_xTemp;
+		FDMVector2 m_residual;
+
+		void Relax(FDMLinearSystem2* system, FDMVector2* xTemp);
+	};
+
+	//! Shared pointer type for the FDMJacobiSolver2.
+	using FDMJacobiSolver2Ptr = std::shared_ptr<FDMJacobiSolver2>;
+}
+
+#endif
