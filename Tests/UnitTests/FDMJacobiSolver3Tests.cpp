1b99cdc0556efd0274b410b9f5041b02ce60be2c
Chris Ohk <utilforever@gmail.com>
2017-08-20 17:16:51 +0900

    Upload FDMJacobiSolver3Tests.cpp - Unit tests for FDMJacobiSolver3

---
+++ Tests/UnitTests/FDMJacobiSolver3Tests.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+
+#include <Solver/FDM/FDMJacobiSolver3.h>
+
+using namespace CubbyFlow;
+
+TEST(FDMJacobiSolver3, Constructors)
+{
+	FDMLinearSystem3 system;
+	system.A.Resize(3, 3, 3);
+	system.x.Resize(3, 3, 3);
+	system.b.Resize(3, 3, 3);
+
+	system.A.ForEachIndex([&](size_t i, size_t j, size_t k)
+	{
+		if (i > 0)
+		{
+			system.A(i, j, k).center += 1.0;
+		}
+		if (i < system.A.Width() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).right -= 1.0;
+		}
+
+		if (j > 0) {
+			system.A(i, j, k).center += 1.0;
+		}
+		else {
+			system.b(i, j, k) += 1.0;
+		}
+
+		if (j < system.A.Height() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).up -= 1.0;
+		}
+		else
+		{
+			system.b(i, j, k) -= 1.0;
+		}
+
+		if (k > 0)
+		{
+			system.A(i, j, k).center += 1.0;
+		}
+		if (k < system.A.Depth() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).front -= 1.0;
+		}
+	});
+
+	FDMJacobiSolver3 solver(100, 10, 1e-9);
+	solver.Solve(&system);
+
+	EXPECT_GT(solver.GetTolerance(), solver.GetLastResidual());
+}
