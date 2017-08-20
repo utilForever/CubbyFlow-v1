e954280413f290071ec55c415d7b9c134a7cf315
Chris Ohk <utilforever@gmail.com>
2017-08-20 16:55:36 +0900

    Upload FDMICCGSolver3Tests.cpp - Unit tests for FDMICCGSolver3

---
+++ Tests/UnitTests/FDMICCGSolver3Tests.cpp
@@ -0,0 +1,60 @@
+#include "pch.h"
+
+#include <Solver/FDM/FDMICCGSolver3.h>
+
+using namespace CubbyFlow;
+
+TEST(FDMICCGSolver3, Constructors)
+{
+	FDMLinearSystem3 system;
+	system.A.Resize(3, 3, 3);
+	system.x.Resize(3, 3, 3);
+	system.b.Resize(3, 3, 3);
+
+	system.A.ForEachIndex([&](size_t i, size_t j, size_t k)
+	{
+		if (i > 0)
+		{
+			system.A(i, j, k).center += 1.0;
+		}
+		if (i < system.A.Width() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).right -= 1.0;
+		}
+
+		if (j > 0)
+		{
+			system.A(i, j, k).center += 1.0;
+		}
+		else
+		{
+			system.b(i, j, k) += 1.0;
+		}
+
+		if (j < system.A.Height() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).up -= 1.0;
+		}
+		else
+		{
+			system.b(i, j, k) -= 1.0;
+		}
+
+		if (k > 0)
+		{
+			system.A(i, j, k).center += 1.0;
+		}
+		if (k < system.A.Depth() - 1)
+		{
+			system.A(i, j, k).center += 1.0;
+			system.A(i, j, k).front -= 1.0;
+		}
+	});
+
+	FDMICCGSolver3 solver(100, 1e-9);
+	solver.Solve(&system);
+
+	EXPECT_GT(solver.GetTolerance(), solver.GetLastResidual());
+}
