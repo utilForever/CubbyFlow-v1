659671a9ced59f4f64d35133e37b61ad1af7ce11
Chris Ohk <utilforever@gmail.com>
2017-08-20 16:51:42 +0900

    Upload FDMICCGSolver2Tests.cpp - Unit tests for FDMICCGSolver2

---
+++ Tests/UnitTests/FDMICCGSolver2Tests.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+
+#include <Solver/FDM/FDMICCGSolver2.h>
+
+using namespace CubbyFlow;
+
+TEST(FDMICCGSolver2, Constructors)
+{
+	FDMLinearSystem2 system;
+	system.A.Resize(3, 3);
+	system.x.Resize(3, 3);
+	system.b.Resize(3, 3);
+
+	system.A.ForEachIndex([&](size_t i, size_t j)
+	{
+		if (i > 0)
+		{
+			system.A(i, j).center += 1.0;
+		}
+		if (i < system.A.Width() - 1)
+		{
+			system.A(i, j).center += 1.0;
+			system.A(i, j).right -= 1.0;
+		}
+
+		if (j > 0)
+		{
+			system.A(i, j).center += 1.0;
+		}
+		else
+		{
+			system.b(i, j) += 1.0;
+		}
+
+		if (j < system.A.Height() - 1)
+		{
+			system.A(i, j).center += 1.0;
+			system.A(i, j).up -= 1.0;
+		}
+		else
+		{
+			system.b(i, j) -= 1.0;
+		}
+	});
+
+	FDMICCGSolver2 solver(10, 1e-9);
+	solver.Solve(&system);
+
+	EXPECT_GT(solver.GetTolerance(), solver.GetLastResidual());
+}
