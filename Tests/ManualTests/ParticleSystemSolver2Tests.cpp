b4b966ae5477b3bb47c7835470b6c503f6101bc9
Chris Ohk <utilforever@gmail.com>
2017-08-23 12:57:54 +0900

    Upload ParticleSystemSolver2Tests.cpp - Manual tests for ParticleSystemSolver2

---
+++ Tests/ManualTests/ParticleSystemSolver2Tests.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+
+#include <ManualTests.h>
+
+#include <Collider/RigidBodyCollider2.h>
+#include <Emitter/PointParticleEmitter2.h>
+#include <Field/ConstantVectorField2.h>
+#include <Geometry/Plane2.h>
+#include <Solver/Particle/ParticleSystemSolver2.h>
+
+using namespace CubbyFlow;
+
+CUBBYFLOW_TESTS(ParticleSystemSolver2);
+
+CUBBYFLOW_BEGIN_TEST_F(ParticleSystemSolver2, Update)
+{
+	Plane2Ptr plane = std::make_shared<Plane2>(Vector2D(0, 1), Vector2D());
+	RigidBodyCollider2Ptr collider = std::make_shared<RigidBodyCollider2>(plane);
+	ConstantVectorField2Ptr wind = std::make_shared<ConstantVectorField2>(Vector2D(1, 0));
+
+	ParticleSystemSolver2 solver;
+	solver.SetCollider(collider);
+	solver.SetWind(wind);
+
+	ParticleSystemData2Ptr particles = solver.GetParticleSystemData();
+	PointParticleEmitter2Ptr emitter = std::make_shared<PointParticleEmitter2>(Vector2D(0, 3), Vector2D(0, 1), 5.0, 45.0);
+	emitter->SetMaxNumberOfNewParticlesPerSecond(100);
+	solver.SetEmitter(emitter);
+
+	SaveParticleDataXY(particles, 0);
+
+	Frame frame(1, 1.0 / 60.0);
+	for (; frame.index < 360; frame.Advance())
+	{
+		solver.Update(frame);
+
+		SaveParticleDataXY(particles, frame.index);
+	}
+}
+CUBBYFLOW_END_TEST_F
