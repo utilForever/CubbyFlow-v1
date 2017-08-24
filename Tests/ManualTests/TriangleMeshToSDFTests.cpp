c9c891900c2c9dc27a2044031cee1ca72e51b0b2
Chris Ohk <utilforever@gmail.com>
2017-08-24 13:12:34 +0900

    Upload TriangleMeshToSDFTests.cpp - Manual tests for TriangleMeshToSDF

---
+++ Tests/ManualTests/TriangleMeshToSDFTests.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+
+#include <ManualTests.h>
+
+#include <Array/Array2.h>
+#include <Geometry/TriangleMeshToSDF.h>
+#include <Grid/VertexCenteredScalarGrid3.h>
+#include <MarchingCubes/MarchingCubes.h>
+
+using namespace CubbyFlow;
