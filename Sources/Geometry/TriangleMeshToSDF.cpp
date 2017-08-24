9520e8b1a51fd56a882d0cd89c9b5559ceba2b8d
Chris Ohk <utilforever@gmail.com>
2017-08-24 23:42:33 +0900

    Update TriangleMeshToSDF.cpp - Refactor code

--- Sources/Geometry/TriangleMeshToSDF.cpp
+++ Sources/Geometry/TriangleMeshToSDF.cpp
@@ -40,12 +40,8 @@ namespace CubbyFlow
 	static void CheckNeighbor(
 		const TriangleMesh3& mesh,
 		const Vector3D& gx,
-		ssize_t i0,
-		ssize_t j0,
-		ssize_t k0,
-		ssize_t i1,
-		ssize_t j1,
-		ssize_t k1,
+		ssize_t i0,	ssize_t j0, ssize_t k0,
+		ssize_t i1, ssize_t j1, ssize_t k1,
 		ScalarGrid3* sdf,
 		Array3<size_t>* closestTri)
 	{
@@ -53,23 +49,22 @@ namespace CubbyFlow
 		{
 			size_t t = (*closestTri)(i1, j1, k1);
 			Triangle3 tri = mesh.Triangle(t);
-
 			double d = tri.ClosestDistance(gx);
 
-			if (d < (*sdf)(i0, j0, k0)) {
+			if (d < (*sdf)(i0, j0, k0))
+			{
 				(*sdf)(i0, j0, k0) = d;
 				(*closestTri)(i0, j0, k0) = (*closestTri)(i1, j1, k1);
 			}
 		}
 	}
 
-	static void sweep(
+	static void Sweep(
 		const TriangleMesh3& mesh,
-		int di,
-		int dj,
-		int dk,
+		int di, int dj, int dk,
 		ScalarGrid3* sdf,
-		Array3<size_t>* closestTri) {
+		Array3<size_t>* closestTri)
+	{
 		Size3 size = sdf->GetDataSize();
 		Vector3D h = sdf->GridSpacing();
 		Vector3D origin = sdf->GetDataOrigin();
@@ -79,102 +74,110 @@ namespace CubbyFlow
 		ssize_t nk = static_cast<ssize_t>(size.z);
 
 		ssize_t i0, i1;
-		if (di > 0) {
+		if (di > 0)
+		{
 			i0 = 1;
 			i1 = ni;
 		}
-		else {
+		else
+		{
 			i0 = ni - 2;
 			i1 = -1;
 		}
 
 		ssize_t j0, j1;
-		if (dj > 0) {
+		if (dj > 0)
+		{
 			j0 = 1;
 			j1 = nj;
 		}
-		else {
+		else
+		{
 			j0 = nj - 2;
 			j1 = -1;
 		}
 
 		ssize_t k0, k1;
-		if (dk > 0) {
+		if (dk > 0)
+		{
 			k0 = 1;
 			k1 = nk;
 		}
-		else {
+		else
+		{
 			k0 = nk - 2;
 			k1 = -1;
 		}
 
-		for (ssize_t k = k0; k != k1; k += dk) {
-			for (ssize_t j = j0; j != j1; j += dj) {
-				for (ssize_t i = i0; i != i1; i += di) {
+		for (ssize_t k = k0; k != k1; k += dk)
+		{
+			for (ssize_t j = j0; j != j1; j += dj)
+			{
+				for (ssize_t i = i0; i != i1; i += di)
+				{
 					Vector3D gx({ i, j, k });
 					gx *= h;
 					gx += origin;
 
-					CheckNeighbor(
-						mesh, gx, i, j, k, i - di, j, k, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i, j - dj, k, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i - di, j - dj, k, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i, j, k - dk, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i - di, j, k - dk, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i, j - dj, k - dk, sdf, closestTri);
-					CheckNeighbor(
-						mesh, gx, i, j, k, i - di, j - dj, k - dk, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i - di, j, k, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i, j - dj, k, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i - di, j - dj, k, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i, j, k - dk, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i - di, j, k - dk, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i, j - dj, k - dk, sdf, closestTri);
+					CheckNeighbor(mesh, gx, i, j, k, i - di, j - dj, k - dk, sdf, closestTri);
 				}
 			}
 		}
 	}
 
 	// calculate twice signed area of triangle (0,0)-(x1,y1)-(x2,y2)
-	// return an SOS-determined sign (-1, +1, or 0 only if it's a truly degenerate
-	// triangle)
-	static int orientation(
-		double x1,
-		double y1,
-		double x2,
-		double y2,
-		double* twiceSignedArea) {
+	// return an SOS-determined sign (-1, +1, or 0 only if it's a truly degenerate triangle)
+	static int Orientation(
+		double x1, double y1,
+		double x2, double y2,
+		double* twiceSignedArea)
+	{
 		(*twiceSignedArea) = y1 * x2 - x1 * y2;
 
-		if ((*twiceSignedArea) > 0) {
+		if ((*twiceSignedArea) > 0)
+		{
 			return 1;
 		}
-		else if ((*twiceSignedArea) < 0) {
+		if ((*twiceSignedArea) < 0)
+		{
 			return -1;
 		}
-		else if (y2 > y1) {
+		if (y2 > y1)
+		{
 			return 1;
 		}
-		else if (y2 < y1) {
+		if (y2 < y1)
+		{
 			return -1;
 		}
-		else if (x1 > x2) {
+		if (x1 > x2)
+		{
 			return 1;
 		}
-		else if (x1 < x2) {
+		if (x1 < x2)
+		{
 			return -1;
 		}
-		else {
-			// only true when x1==x2 and y1==y2
-			return 0;
-		}
+
+		// only true when x1==x2 and y1==y2
+		return 0;
 	}
 
 	// robust test of (x0,y0) in the triangle (x1,y1)-(x2,y2)-(x3,y3)
 	// if true is returned, the barycentric coordinates are set in a,b,c.
-	static bool pointInTriangle2D(
+	static bool PointInTriangle2D(
 		double x0, double y0,
-		double x1, double y1, double x2, double y2, double x3, double y3,
-		double* a, double* b, double* c) {
+		double x1, double y1,
+		double x2, double y2,
+		double x3, double y3,
+		double* a, double* b, double* c)
+	{
 		x1 -= x0;
 		x2 -= x0;
 		x3 -= x0;
@@ -182,39 +185,41 @@ namespace CubbyFlow
 		y2 -= y0;
 		y3 -= y0;
 
-		int signa = orientation(x2, y2, x3, y3, a);
-		if (signa == 0) {
+		int signa = Orientation(x2, y2, x3, y3, a);
+		if (signa == 0)
+		{
 			return false;
 		}
 
-		int signb = orientation(x3, y3, x1, y1, b);
-		if (signb != signa) {
+		int signb = Orientation(x3, y3, x1, y1, b);
+		if (signb != signa)
+		{
 			return false;
 		}
 
-		int signc = orientation(x1, y1, x2, y2, c);
-		if (signc != signa) {
+		int signc = Orientation(x1, y1, x2, y2, c);
+		if (signc != signa)
+		{
 			return false;
 		}
 
 		double sum = (*a) + (*b) + (*c);
 
-		// if the SOS signs match and are nonzero, there's no way all of a, b, and c
-		// are zero.
+		// if the SOS signs match and are nonzero, there's no way all of a, b, and c are zero.
 		assert(sum != 0);
 
 		*a /= sum;
 		*b /= sum;
 		*c /= sum;
+
 		return true;
 	}
 
-	void triangleMeshToSdf(
-		const TriangleMesh3& mesh,
-		ScalarGrid3* sdf,
-		const unsigned int exactBand) {
+	void TriangleMeshToSDF(const TriangleMesh3& mesh, ScalarGrid3* sdf, const unsigned int exactBand)
+	{
 		Size3 size = sdf->GetDataSize();
-		if (size.x * size.y * size.z == 0) {
+		if (size.x * size.y * size.z == 0)
+		{
 			return;
 		}
 
@@ -237,7 +242,9 @@ namespace CubbyFlow
 		ssize_t maxSizeX = static_cast<ssize_t>(size.x);
 		ssize_t maxSizeY = static_cast<ssize_t>(size.y);
 		ssize_t maxSizeZ = static_cast<ssize_t>(size.z);
-		for (size_t t = 0; t < nTri; ++t) {
+
+		for (size_t t = 0; t < nTri; ++t)
+		{
 			Point3UI indices = mesh.PointIndex(t);
 
 			Triangle3 tri = mesh.Triangle(t);
@@ -267,12 +274,17 @@ namespace CubbyFlow
 			ssize_t k1 = static_cast<ssize_t>(std::max({ f1.z, f2.z, f3.z }));
 			k1 = std::clamp(k1 + bandwidth + 1, ZERO_SSIZE, maxSizeZ - 1);
 
-			for (ssize_t k = k0; k <= k1; ++k) {
-				for (ssize_t j = j0; j <= j1; ++j) {
-					for (ssize_t i = i0; i <= i1; ++i) {
+			for (ssize_t k = k0; k <= k1; ++k)
+			{
+				for (ssize_t j = j0; j <= j1; ++j)
+				{
+					for (ssize_t i = i0; i <= i1; ++i)
+					{
 						Vector3D gx = gridPos(i, j, k);
 						double d = tri.ClosestDistance(gx);
-						if (d < (*sdf)(i, j, k)) {
+
+						if (d < (*sdf)(i, j, k))
+						{
 							(*sdf)(i, j, k) = d;
 							closestTri(i, j, k) = t;
 						}
@@ -290,53 +302,65 @@ namespace CubbyFlow
 			k1 = static_cast<ssize_t>(std::floor(std::max({ f1.z, f2.z, f3.z })));
 			k1 = std::clamp(k1 + bandwidth + 1, ZERO_SSIZE, maxSizeZ - 1);
 
-			for (ssize_t k = k0; k <= k1; ++k) {
-				for (ssize_t j = j0; j <= j1; ++j) {
+			for (ssize_t k = k0; k <= k1; ++k)
+			{
+				for (ssize_t j = j0; j <= j1; ++j)
+				{
 					double a, b, c;
 					double jD = static_cast<double>(j);
 					double kD = static_cast<double>(k);
-					if (pointInTriangle2D(
-						jD, kD, f1.y, f1.z, f2.y, f2.z, f3.y, f3.z, &a, &b, &c)) {
+
+					if (PointInTriangle2D(jD, kD, f1.y, f1.z, f2.y, f2.z, f3.y, f3.z, &a, &b, &c))
+					{
 						// intersection i coordinate
 						double fi = a * f1.x + b * f2.x + c * f3.x;
 
 						// intersection is in (iInterval - 1, iInterval]
 						int iInterval = static_cast<int>(std::ceil(fi));
-						if (iInterval < 0) {
+						if (iInterval < 0)
+						{
 							// we enlarge the first interval to include everything
 							// to the -x direction
 							++intersectionCount(0, j, k);
 						}
-						else if (iInterval < static_cast<int>(size.x)) {
+						else if (iInterval < static_cast<int>(size.x))
+						{
 							++intersectionCount(iInterval, j, k);
 						}
-						// we ignore intersections that are beyond the +x side of
-						// the grid
+
+						// we ignore intersections that are beyond the +x side of the grid
 					}
 				}
 			}
 		}
 
 		// and now we fill in the rest of the distances with fast sweeping
-		for (unsigned int pass = 0; pass < 2; ++pass) {
-			sweep(mesh, +1, +1, +1, sdf, &closestTri);
-			sweep(mesh, -1, -1, -1, sdf, &closestTri);
-			sweep(mesh, +1, +1, -1, sdf, &closestTri);
-			sweep(mesh, -1, -1, +1, sdf, &closestTri);
-			sweep(mesh, +1, -1, +1, sdf, &closestTri);
-			sweep(mesh, -1, +1, -1, sdf, &closestTri);
-			sweep(mesh, +1, -1, -1, sdf, &closestTri);
-			sweep(mesh, -1, +1, +1, sdf, &closestTri);
+		for (unsigned int pass = 0; pass < 2; ++pass)
+		{
+			Sweep(mesh, +1, +1, +1, sdf, &closestTri);
+			Sweep(mesh, -1, -1, -1, sdf, &closestTri);
+			Sweep(mesh, +1, +1, -1, sdf, &closestTri);
+			Sweep(mesh, -1, -1, +1, sdf, &closestTri);
+			Sweep(mesh, +1, -1, +1, sdf, &closestTri);
+			Sweep(mesh, -1, +1, -1, sdf, &closestTri);
+			Sweep(mesh, +1, -1, -1, sdf, &closestTri);
+			Sweep(mesh, -1, +1, +1, sdf, &closestTri);
 		}
 
 		// then figure out signs (inside/outside) from intersection counts
-		for (size_t k = 0; k < size.z; ++k) {
-			for (size_t j = 0; j < size.y; ++j) {
+		for (size_t k = 0; k < size.z; ++k)
+		{
+			for (size_t j = 0; j < size.y; ++j)
+			{
 				unsigned int totalCount = 0U;
-				for (size_t i = 0; i < size.x; ++i) {
+
+				for (size_t i = 0; i < size.x; ++i)
+				{
 					totalCount += intersectionCount(i, j, k);
+
 					// if parity of intersections so far is odd,
-					if (totalCount % 2 == 1) {
+					if (totalCount % 2 == 1)
+					{
 						// we are inside the mesh
 						(*sdf)(i, j, k) = -(*sdf)(i, j, k);
 					}
