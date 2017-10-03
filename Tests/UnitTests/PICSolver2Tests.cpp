d1208fd84c258fd204e155c067cfa4e957714389
Chris Ohk <utilforever@gmail.com>
2017-10-03 11:52:22 +0900

    Upload PICSolversTests - Add unit/manual tests for some missing solvers (#171)

---
+++ Tests/UnitTests/PICSolver2Tests.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+#include <Solver/PIC/PICSolver2.h>
+
+using namespace CubbyFlow;
+
+TEST(PICSolver2, UpdateEmpty)
+{
+	PICSolver2 solver;
+
+	for (Frame frame; frame.index < 2; ++frame)
+	{
+		solver.Update(frame);
+	}
+}
