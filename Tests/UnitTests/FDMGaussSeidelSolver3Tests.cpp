8d4d5572d9b425401311959f276d7f9b658a147d
Chris Ohk <utilforever@gmail.com>
2017-11-02 00:56:26 +0900

    Update FDMGaussSeidelSolver3Tests - Add unit tests for Relax/RelaxRedBlack (#193)

--- Tests/UnitTests/FDMGaussSeidelSolver3Tests.cpp
+++ Tests/UnitTests/FDMGaussSeidelSolver3Tests.cpp
@@ -1,60 +1,81 @@
 #include "pch.h"
+#include "FDMLinearSystemSolverTestHelper3.h"
 
 #include <Solver/FDM/FDMGaussSeidelSolver3.h>
 
 using namespace CubbyFlow;
 
-TEST(FDMGaussSeidelSolver3, Constructors)
+TEST(FDMGaussSeidelSolver3, SolveLowRes)
 {
 	FDMLinearSystem3 system;
-	system.A.Resize(3, 3, 3);
-	system.x.Resize(3, 3, 3);
-	system.b.Resize(3, 3, 3);
-
-	system.A.ForEachIndex([&](size_t i, size_t j, size_t k)
-	{
-		if (i > 0)
-		{
-			system.A(i, j, k).center += 1.0;
-		}
-		if (i < system.A.Width() - 1)
-		{
-			system.A(i, j, k).center += 1.0;
-			system.A(i, j, k).right -= 1.0;
-		}
-
-		if (j > 0)
-		{
-			system.A(i, j, k).center += 1.0;
-		}
-		else
-		{
-			system.b(i, j, k) += 1.0;
-		}
-
-		if (j < system.A.Height() - 1)
-		{
-			system.A(i, j, k).center += 1.0;
-			system.A(i, j, k).up -= 1.0;
-		}
-		else
-		{
-			system.b(i, j, k) -= 1.0;
-		}
-
-		if (k > 0)
-		{
-			system.A(i, j, k).center += 1.0;
-		}
-		if (k < system.A.Depth() - 1)
-		{
-			system.A(i, j, k).center += 1.0;
-			system.A(i, j, k).front -= 1.0;
-		}
-	});
+	FDMLinearSystemSolverTestHelper3::BuildTestLinearSystem(&system, { 3, 3, 3 });
 
 	FDMGaussSeidelSolver3 solver(100, 10, 1e-9);
 	solver.Solve(&system);
 
 	EXPECT_GT(solver.GetTolerance(), solver.GetLastResidual());
+}
+
+TEST(FDMGaussSeidelSolver3, Solve)
+{
+	FDMLinearSystem3 system;
+	FDMLinearSystemSolverTestHelper3::BuildTestLinearSystem(&system, { 32, 32, 32 });
+
+	auto buffer = system.x;
+	FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+	double norm0 = FDMBlas3::L2Norm(buffer);
+
+	FDMGaussSeidelSolver3 solver(100, 10, 1e-9);
+	solver.Solve(&system);
+
+	FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+	double norm1 = FDMBlas3::L2Norm(buffer);
+
+	EXPECT_LT(norm1, norm0);
+}
+
+TEST(FDMGaussSeidelSolver3, Relax)
+{
+	FDMLinearSystem3 system;
+	FDMLinearSystemSolverTestHelper3::BuildTestLinearSystem(&system, { 32, 32, 32 });
+
+	auto buffer = system.x;
+	FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+	double norm0 = FDMBlas3::L2Norm(buffer);
+
+	for (int i = 0; i < 200; ++i)
+	{
+		FDMGaussSeidelSolver3::Relax(system.A, system.b, 1.0, &system.x);
+
+		FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+		double norm = FDMBlas3::L2Norm(buffer);
+		EXPECT_LT(norm, norm0);
+
+		norm0 = norm;
+	}
+}
+
+TEST(FDMGaussSeidelSolver3, RelaxRedBlack)
+{
+	FDMLinearSystem3 system;
+	FDMLinearSystemSolverTestHelper3::BuildTestLinearSystem(&system, { 32, 32, 32 });
+
+	auto buffer = system.x;
+	FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+	double norm0 = FDMBlas3::L2Norm(buffer);
+
+	for (int i = 0; i < 200; ++i)
+	{
+		FDMGaussSeidelSolver3::RelaxRedBlack(system.A, system.b, 1.0, &system.x);
+
+		FDMBlas3::Residual(system.A, system.x, system.b, &buffer);
+		double norm = FDMBlas3::L2Norm(buffer);
+		
+		if (i > 0)
+		{
+			EXPECT_LT(norm, norm0);
+		}
+
+		norm0 = norm;
+	}
 }
