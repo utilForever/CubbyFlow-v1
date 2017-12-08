8f5214944553ee1d163d30f20cae8cd6d9ecb2a7
Chris Ohk <utilForever@gmail.com>
2017-12-08 23:47:34 +0900

    Update VolumeGridEmitter - Add UNUSED_VARIABLE macro (#209)

--- Sources/Emitter/VolumeGridEmitter3.cpp
+++ Sources/Emitter/VolumeGridEmitter3.cpp
@@ -11,6 +11,7 @@
 #include <Grid/FaceCenteredGrid3.h>
 #include <LevelSet/LevelSetUtils.h>
 #include <Surface/Implicit/SurfaceToImplicit3.h>
+#include <Utils/Macros.h>
 
 namespace CubbyFlow
 {
@@ -69,6 +70,9 @@ namespace CubbyFlow
 
 	void VolumeGridEmitter3::OnUpdate(double currentTimeInSeconds, double timeIntervalInSeconds)
 	{
+		UNUSED_VARIABLE(currentTimeInSeconds);
+		UNUSED_VARIABLE(timeIntervalInSeconds);
+
 		if (m_hasEmitted && m_isOneShot)
 		{
 			return;
