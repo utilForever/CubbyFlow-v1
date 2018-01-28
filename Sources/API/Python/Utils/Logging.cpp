2b00937f70bcd44002a465a75bfe924f9eecb835
Chris Ohk <utilForever@gmail.com>
2018-01-28 23:49:27 +0900

    Update Logging - Clean up whitespaces (#236)

--- Sources/API/Python/Utils/Logging.cpp
+++ Sources/API/Python/Utils/Logging.cpp
@@ -16,16 +16,16 @@ using namespace CubbyFlow;
 void AddLogging(pybind11::module& m)
 {
 	pybind11::enum_<LogLevel>(m, "LogLevel")
-		.value("ALL",	LogLevel::All)
-		.value("DEBUG",	LogLevel::Debug)
-		.value("INFO",	LogLevel::Info)
-		.value("WARN",	LogLevel::Warn)
-		.value("ERROR",	LogLevel::Error)
-		.value("OFF",	LogLevel::Off)
-		.export_values();
+	.value("ALL",	LogLevel::All)
+	.value("DEBUG",	LogLevel::Debug)
+	.value("INFO",	LogLevel::Info)
+	.value("WARN",	LogLevel::Warn)
+	.value("ERROR",	LogLevel::Error)
+	.value("OFF",	LogLevel::Off)
+	.export_values();
 
 	pybind11::class_<Logging>(m, "Logging")
-		.def_static("SetLevel", &Logging::SetLevel)
-		.def_static("Mute",		&Logging::Mute)
-		.def_static("Unmute",	&Logging::Unmute);
+	.def_static("SetLevel", &Logging::SetLevel)
+	.def_static("Mute",		&Logging::Mute)
+	.def_static("Unmute",	&Logging::Unmute);
 }
