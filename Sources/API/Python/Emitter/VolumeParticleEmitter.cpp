b00ef8ca0014024aabce8ede7ab99bf48da355d9
Chris Ohk <utilForever@gmail.com>
2018-02-11 17:04:54 +0900

    Upload VolumeParticleEmitter - VolumeParticleEmitter functions for CubbyFlow Python API (#236)

---
+++ Sources/API/Python/Emitter/VolumeParticleEmitter.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+> File Name: VolumeParticleEmitter.cpp
+> Project Name: CubbyFlow
+> Author: Chan-Ho Chris Ohk
+> Purpose: VolumeParticleEmitter functions for CubbyFlow Python API.
+> Created Time: 2018/02/11
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <API/Python/Emitter/VolumeParticleEmitter.h>
+#include <Core/Emitter/VolumeParticleEmitter2.h>
+#include <Core/Emitter/VolumeParticleEmitter3.h>
+
+#include <pybind11/pybind11.h>
+
+using namespace CubbyFlow;
+
