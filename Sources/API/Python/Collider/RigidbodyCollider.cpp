a9ba6198b45997f8a8b8425839e9c00a734a9d72
Chris Ohk <utilForever@gmail.com>
2018-02-25 15:20:29 +0900

    Update RigidbodyCollider - Fix compile error (#239)

--- Sources/API/Python/Collider/RigidbodyCollider.cpp
+++ Sources/API/Python/Collider/RigidbodyCollider.cpp
@@ -8,8 +8,8 @@
 *************************************************************************/
 #include <API/Python/Collider/RigidbodyCollider.h>
 #include <API/Python/Utils/pybind11Utils.h>
-#include <Core/Collider/RigidbodyCollider2.h>
-#include <Core/Collider/RigidbodyCollider3.h>
+#include <Core/Collider/RigidBodyCollider2.h>
+#include <Core/Collider/RigidBodyCollider3.h>
 
 #include <pybind11/pybind11.h>
 
