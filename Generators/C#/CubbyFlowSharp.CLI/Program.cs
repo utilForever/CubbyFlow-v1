76614c0be15df765c44585d169230cd25e4a652a
Chris Ohk <utilForever@gmail.com>
2018-03-07 23:51:19 +0900

    Upload CubbyFlowSharp - Create CppSharp binding project for making C# API (#256)

---
+++ Generators/C#/CubbyFlowSharp.CLI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace CubbyFlowSharp.CLI
+{
+    class CubbyFlow : ILibrary
+    {
+        public void Setup(Driver driver)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string includePath = System.IO.Path.Combine(path, @"../../../../../Includes");
+
+            var options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+
+            var module = options.AddModule("CubbyFlowSharp");
+            module.IncludeDirs.Add(includePath);
+            module.Headers.Add("Core/Animation/Animation.h");
+            module.Headers.Add("Core/Animation/Frame.h");
+            module.Headers.Add("Core/Animation/PhysicsAnimation.h");
+            module.LibraryDirs.Add(path);
+            module.Libraries.Add("CubbyFlow.lib");
+        }
+
+        public void SetupPasses(Driver driver)
+        {
+
+        }
+
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+
+        }
+
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleDriver.Run(new CubbyFlow());
+        }
+    }
+}
