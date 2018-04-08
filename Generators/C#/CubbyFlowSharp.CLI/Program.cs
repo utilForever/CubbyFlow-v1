d7cb1dd670302d3f3d3594e89d81f5da5aa26374
Chris Ohk <utilForever@gmail.com>
2018-04-08 20:45:15 +0900

    Update CubbyFlowSharp.CLI - Create CubbyFlowSharp.CLI project for making C++/CLI code (#256)
    Add header files from CubbyFlow and change some options

--- Generators/C#/CubbyFlowSharp.CLI/Program.cs
+++ Generators/C#/CubbyFlowSharp.CLI/Program.cs
@@ -2,6 +2,7 @@
 using CppSharp;
 using CppSharp.AST;
 using CppSharp.Generators;
+using CppSharp.Parser;
 
 namespace CubbyFlowSharp.CLI
 {
@@ -12,14 +13,37 @@ namespace CubbyFlowSharp.CLI
             string path = Directory.GetCurrentDirectory();
             string includePath = System.IO.Path.Combine(path, @"../../../../../Includes");
 
-            var options = driver.Options;
-            options.GeneratorKind = GeneratorKind.CSharp;
+            var driverOptions = driver.Options;
+            driverOptions.GeneratorKind = GeneratorKind.CLI;
 
-            var module = options.AddModule("CubbyFlowSharp");
+            var parserOptions = driver.ParserOptions;
+            parserOptions.AddArguments("-fexceptions");
+            parserOptions.EnableRTTI = true;
+
+            var module = driverOptions.AddModule("CubbyFlowSharp");
             module.IncludeDirs.Add(includePath);
+
             module.Headers.Add("Core/Animation/Animation.h");
             module.Headers.Add("Core/Animation/Frame.h");
             module.Headers.Add("Core/Animation/PhysicsAnimation.h");
+
+            module.Headers.Add("Core/Array/ArrayAccessor.h");
+            module.Headers.Add("Core/Array/ArrayAccessor1.h");
+            module.Headers.Add("Core/Array/ArrayAccessor1-Impl.h");
+            module.Headers.Add("Core/Array/ArrayAccessor2.h");
+            module.Headers.Add("Core/Array/ArrayAccessor2-Impl.h");
+            module.Headers.Add("Core/Array/ArrayAccessor3.h");
+            module.Headers.Add("Core/Array/ArrayAccessor3-Impl.h");
+
+            module.Headers.Add("Core/Point/Point.h");
+            module.Headers.Add("Core/Point/Point-Impl.h");
+            module.Headers.Add("Core/Point/Point2.h");
+            module.Headers.Add("Core/Point/Point2-Impl.h");
+            module.Headers.Add("Core/Point/Point3.h");
+            module.Headers.Add("Core/Point/Point3-Impl.h");
+
+            module.Headers.Add("Core/Utils/Constants.h");
+
             module.LibraryDirs.Add(path);
             module.Libraries.Add("CubbyFlow.lib");
         }
