c21bcc9c750617344fc2e04e216a50f11beb61a6
Chris Ohk <utilForever@gmail.com>
2018-04-09 01:29:04 +0900

    Upload CubbyFlowSharp.Tests - Add project for C# API unit tests (#260)

---
+++ Generators/C#/CubbyFlowSharp.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubbyFlowSharp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        class MyAnimation : CubbyFlow.Animation
+        {
+            public CubbyFlow.Frame LastFrame;
+
+            public void OnUpdate(CubbyFlow.Frame frame)
+            {
+                LastFrame = frame;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var f = new CubbyFlow.Frame(3, 0.02);
+            var anim = new MyAnimation();
+
+            anim.Update(f);
+
+            Assert.AreEqual(anim.LastFrame.Index, 3);
+            Assert.AreEqual(anim.LastFrame.TimeIntervalInSeconds, 0.02);
+        }
+    }
+}
