8d319bff403cdb3f7d45651f45cf159d03abc048
Chris Ohk <utilForever@gmail.com>
2018-04-12 17:14:04 +0900

    Upload Animation - Animation class for CubbyFlow C# API (#261)

---
+++ Includes/API/CSharp/Animation/Animation.h
@@ -0,0 +1,44 @@
+///*************************************************************************
+//> File Name: Animation.h
+//> Project Name: CubbyFlow
+//> Author: Chan-Ho Chris Ohk
+//> Purpose: Animation class for CubbyFlow C# API.
+//> Created Time: 2018/04/12
+//> Copyright (c) 2018, Chan-Ho Chris Ohk
+//*************************************************************************/
+//#ifndef CUBBYFLOW_CSHARP_ANIMATION_H
+//#define CUBBYFLOW_CSHARP_ANIMATION_H
+//
+//#include <Core/Animation/Animation.h>
+//#include <API/CSharp/Animation/Frame.h>
+//
+//namespace CubbyFlowSharp
+//{
+//	//!
+//	//! \brief Abstract base class for animation-related class.
+//	//!
+//	//! This class represents the animation logic in very abstract level.
+//	//! Generally animation is a function of time and/or its previous state.
+//	//! This base class provides a virtual function update() which can be
+//	//! overriden by its sub-classes to implement their own state update logic.
+//	//!
+//	public ref struct Animation
+//	{
+//		Animation();
+//
+//		virtual ~Animation();
+//
+//		//!
+//		//! \brief Updates animation state for given \p frame.
+//		//!
+//		//! This function updates animation state by calling Animation::onUpdate
+//		//! function.
+//		//!
+//		void Update(Frame^ frame);
+//
+//	protected:
+//		::CubbyFlow::Animation* m_pAnimation;
+//	};
+//}
+//
+//#endif
---
+++ Sources/API/CSharp/Animation/Animation.cpp
@@ -0,0 +1,27 @@
+///*************************************************************************
+//> File Name: Animation.cpp
+//> Project Name: CubbyFlow
+//> Author: Chan-Ho Chris Ohk
+//> Purpose: Animation class for CubbyFlow C# API.
+//> Created Time: 2018/04/12
+//> Copyright (c) 2018, Chan-Ho Chris Ohk
+//*************************************************************************/
+//#include <API/CSharp/Animation/Animation.h>
+//
+//namespace CubbyFlowSharp
+//{
+//	Animation::Animation() : m_pAnimation(new ::CubbyFlow::Animation)
+//	{
+//		// Do nothing
+//	}
+//
+//	Animation::~Animation()
+//	{
+//		// Do nothing
+//	}
+//
+//	void Animation::Update(Frame^ frame)
+//	{
+//		m_pAnimation->Update(frame);
+//	}
+//}
