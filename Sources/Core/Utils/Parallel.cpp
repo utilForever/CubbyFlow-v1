8386339267475a4a0d2958adfe1a937a7ca4776f
Chris Ohk <utilForever@gmail.com>
2018-04-19 23:51:43 +0900

    Update Parallel - Support for multiple tasking systems (#246)

--- Sources/Core/Utils/Parallel.cpp
+++ Sources/Core/Utils/Parallel.cpp
@@ -8,6 +8,14 @@
 *************************************************************************/
 #include <Core/Utils/Parallel.h>
 
+#if defined(CUBBYFLOW_TASKING_TBB)
+#include <tbb/task_arena.h>
+#include <tbb/task_scheduler_init.h>
+#elif defined(CUBBYFLOW_TASKING_OPENMP)
+#include <omp.h>
+#endif
+
+#include <memory>
 #include <thread>
 
 static unsigned int MAX_NUMBER_OF_THREADS = std::thread::hardware_concurrency();
@@ -16,6 +24,21 @@ namespace CubbyFlow
 {
 	void SetMaxNumberOfThreads(unsigned int numThreads)
 	{
+#if defined(CUBBYFLOW_TASKING_TBB)
+		static std::unique_ptr<tbb::task_scheduler_init> tbbInit;
+
+		if (!tbbInit.get())
+		{
+			tbbInit.reset(new tbb::task_scheduler_list(numThreads));
+		}
+		else
+		{
+			tbbInit->terminate();
+			tbbInit->initialize(numThreads);
+		}
+#elif defined(CUBBYFLOW_TASKING_OPENMP)
+		omp_set_num_threads(numThreads);
+#endif
 		MAX_NUMBER_OF_THREADS = std::max(numThreads, 1u);
 	}
 
