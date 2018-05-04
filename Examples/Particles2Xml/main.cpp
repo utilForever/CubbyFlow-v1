5ea310d53791199a09ae1eef2a32a9dca1753a24
Chris Ohk <utilForever@gmail.com>
2018-05-05 00:15:20 +0900

    Update Particle2Xml - Replace getopt library with Clara (#275)

--- Examples/Particles2Xml/main.cpp
+++ Examples/Particles2Xml/main.cpp
@@ -6,137 +6,125 @@
 > Created Time: 2017/09/09
 > Copyright (c) 2018, Chan-Ho Chris Ohk
 *************************************************************************/
+#include <../ClaraUtils.h>
+
 #include <Core/Array/Array1.h>
 #include <Core/Vector/Vector3.h>
 #include <Core/Utils/Serialization.h>
 
-#include <getopt.h>
+#include <Clara/include/clara.hpp>
 
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace CubbyFlow;
 
-void PrintUsage()
-{
-	printf(
-		"Usage: Particles2Xml "
-		"-i input_pos -o output_xml \n"
-		"   -i, --input: input particle position file name\n"
-		"   -o, --output: output obj file name\n"
-		"   -h, --help: print this message\n");
-}
-
 void PrintInfo(size_t numberOfParticles)
 {
-	printf("Number of particles: %zu\n", numberOfParticles);
+    printf("Number of particles: %zu\n", numberOfParticles);
 }
 
 void ParticlesToXML(
-	const Array1<Vector3D>& positions,
-	const std::string& xmlFileName)
+    const Array1<Vector3D>& positions,
+    const std::string& xmlFileName)
 {
-	PrintInfo(positions.size());
+    PrintInfo(positions.size());
 
-	std::ofstream file(xmlFileName.c_str());
-	if (file)
-	{
-		printf("Writing %s...\n", xmlFileName.c_str());
+    std::ofstream file(xmlFileName.c_str());
+    if (file)
+    {
+        printf("Writing %s...\n", xmlFileName.c_str());
 
-		file << "<scene version=\"0.5.0\">";
+        file << "<scene version=\"0.5.0\">";
 
-		for (const auto& pos : positions)
-		{
-			file << "<shape type=\"instance\">";
-			file << "<ref id=\"spheres\"/>";
-			file << "<transform name=\"toWorld\">";
+        for (const auto& pos : positions)
+        {
+            file << "<shape type=\"instance\">";
+            file << "<ref id=\"spheres\"/>";
+            file << "<transform name=\"toWorld\">";
 
-			char buffer[64];
-			snprintf(
-				buffer,
-				sizeof(buffer),
-				"<translate x=\"%f\" y=\"%f\" z=\"%f\"/>",
-				pos.x,
-				pos.y,
-				pos.z);
-			file << buffer;
+            char buffer[64];
+            snprintf(
+                buffer,
+                sizeof(buffer),
+                "<translate x=\"%f\" y=\"%f\" z=\"%f\"/>",
+                pos.x,
+                pos.y,
+                pos.z);
+            file << buffer;
 
-			file << "</transform>";
-			file << "</shape>";
-		}
+            file << "</transform>";
+            file << "</shape>";
+        }
 
-		file << "</scene>";
+        file << "</scene>";
 
-		file.close();
-	}
-	else
-	{
-		printf("Cannot write file %s.\n", xmlFileName.c_str());
-		exit(EXIT_FAILURE);
-	}
+        file.close();
+    }
+    else
+    {
+        printf("Cannot write file %s.\n", xmlFileName.c_str());
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char* argv[])
 {
-	std::string inputFileName;
-	std::string outputFileName;
+    bool showHelp = false;
 
-	// Parse options
-	static struct option longOptions[] =
-	{
-		{ "input",       required_argument,  nullptr,  'i' },
-		{ "output",      required_argument,  nullptr,  'o' },
-		{ "help",        optional_argument,  nullptr,  'h' },
-		{ nullptr,       0,                  nullptr,   0 }
-	};
+    std::string inputFileName;
+    std::string outputFileName;
 
-	int opt;
-	int long_index = 0;
-	while ((opt = getopt_long(argc, argv, "i:o:h", longOptions, &long_index)) != -1)
-	{
-		switch (opt)
-		{
-		case 'i':
-			inputFileName = optarg;
-			break;
-		case 'o':
-			outputFileName = optarg;
-			break;
-		case 'h':
-			PrintUsage();
-			exit(EXIT_SUCCESS);
-		default:
-			PrintUsage();
-			exit(EXIT_FAILURE);
-		}
-	}
+    // Parsing
+    auto parser =
+        clara::Help(showHelp) |
+        clara::Opt(inputFileName, "input_pos")
+        ["-i"]["--input"]
+        ("input particle position file name") |
+        clara::Opt(outputFileName, "output_xml")
+        ["-o"]["--output"]
+        ("output obj file name");
 
-	if (inputFileName.empty() || outputFileName.empty())
-	{
-		PrintUsage();
-		exit(EXIT_FAILURE);
-	}
+    auto result = parser.parse(clara::Args(argc, argv));
+    if (!result)
+    {
+        std::cerr << "Error in command line: " << result.errorMessage() << '\n';
+        exit(EXIT_FAILURE);
+    }
 
-	// Read particle positions
-	Array1<Vector3D> positions;
-	std::ifstream positionFile(inputFileName.c_str(), std::ifstream::binary);
-	if (positionFile)
-	{
-		const std::vector<uint8_t> buffer(
-			(std::istreambuf_iterator<char>(positionFile)),
-			(std::istreambuf_iterator<char>()));
-		Deserialize(buffer, &positions);
-		positionFile.close();
-	}
-	else
-	{
-		printf("Cannot read file %s.\n", inputFileName.c_str());
-		exit(EXIT_FAILURE);
-	}
+    if (showHelp)
+    {
+        std::cout << ToString(parser) << '\n';
+        exit(EXIT_SUCCESS);
+    }
 
-	// Run marching cube and save it to the disk
-	ParticlesToXML(positions, outputFileName);
+    if (inputFileName.empty() || outputFileName.empty())
+    {
+        std::cout << ToString(parser) << '\n';
+        exit(EXIT_FAILURE);
+    }
 
-	return EXIT_SUCCESS;
+    // Read particle positions
+    Array1<Vector3D> positions;
+    std::ifstream positionFile(inputFileName.c_str(), std::ifstream::binary);
+    if (positionFile)
+    {
+        const std::vector<uint8_t> buffer(
+            (std::istreambuf_iterator<char>(positionFile)),
+            (std::istreambuf_iterator<char>()));
+        Deserialize(buffer, &positions);
+        positionFile.close();
+    }
+    else
+    {
+        printf("Cannot read file %s.\n", inputFileName.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    // Run marching cube and save it to the disk
+    ParticlesToXML(positions, outputFileName);
+
+    return EXIT_SUCCESS;
 }
