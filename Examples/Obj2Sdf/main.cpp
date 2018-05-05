3b9e5b8e415cb83b172026875ddd400b8af744fd
Chris Ohk <utilForever@gmail.com>
2018-05-05 10:20:26 +0900

    Update Obj2Sdf - Replace getopt library with Clara (#275)

--- Examples/Obj2Sdf/main.cpp
+++ Examples/Obj2Sdf/main.cpp
@@ -6,164 +6,142 @@
 > Created Time: 2017/09/08
 > Copyright (c) 2018, Chan-Ho Chris Ohk
 *************************************************************************/
+#include <../ClaraUtils.h>
+
 #include <Core/Geometry/TriangleMesh3.h>
 #include <Core/Geometry/TriangleMeshToSDF.h>
 #include <Core/Grid/VertexCenteredScalarGrid3.h>
 #include <Core/MarchingCubes/MarchingCubes.h>
 
-#include <getopt.h>
+#include <Clara/include/clara.hpp>
 
 #include <algorithm>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace CubbyFlow;
 
-void PrintUsage()
-{
-	printf(
-		"Usage: Obj2Sdf "
-		"-i input_obj -o output_sdf -r resolution -m margin_scale\n"
-		"   -i, --input: input obj file name\n"
-		"   -o, --output: output sdf file name\n"
-		"   -r, --resx: grid resolution in x-axis (default: 100)\n"
-		"   -m, --margin: margin scale around the sdf (default: 0.2)\n"
-		"   -h, --help: print this message\n");
-}
-
 void SaveTriangleMeshData(const TriangleMesh3& data, const std::string& fileName)
 {
-	std::ofstream file(fileName.c_str());
-	if (file)
-	{
-		data.WriteObj(&file);
-		file.close();
-	}
+    std::ofstream file(fileName.c_str());
+    if (file)
+    {
+        data.WriteObj(&file);
+        file.close();
+    }
 }
 
 int main(int argc, char* argv[])
 {
-	std::string inputFileName;
-	std::string outputFileName;
-	size_t resolutionX = 100;
-	double marginScale = 0.2;
+    bool showHelp = false;
 
-	// Parse options
-	static struct option longOptions[] =
-	{
-		{ "input",   required_argument,  nullptr,  'i' },
-		{ "output",  required_argument,  nullptr,  'o' },
-		{ "resx",    optional_argument,  nullptr,  'r' },
-		{ "margin",  optional_argument,  nullptr,  'm' },
-		{ "help",    optional_argument,  nullptr,  'h' },
-		{ nullptr,   0,                  nullptr,   0 }
-	};
+    std::string inputFileName;
+    std::string outputFileName;
+    size_t resX = 100;
+    double marginScale = 0.2;
 
-	int opt;
-	int long_index = 0;
-	while ((opt = getopt_long(argc, argv, "i:o:r:m:h", longOptions, &long_index)) != -1)
-	{
-		switch (opt)
-		{
-		case 'i':
-			inputFileName = optarg;
-			break;
-		case 'o':
-			outputFileName = optarg;
-			break;
-		case 'r':
-			resolutionX = static_cast<size_t>(atoi(optarg));
-			break;
-		case 'm':
-			marginScale = std::max(atof(optarg), 0.0);
-			break;
-		case 'h':
-			PrintUsage();
-			exit(EXIT_SUCCESS);
-		default:
-			PrintUsage();
-			exit(EXIT_FAILURE);
-		}
-	}
+    // Parsing
+    auto parser =
+        clara::Help(showHelp) |
+        clara::Opt(inputFileName, "inputFileName")
+        ["-i"]["--input"]
+        ("input obj file name") |
+        clara::Opt(outputFileName, "outputFileName")
+        ["-o"]["--output"]
+        ("output sdf file name") |
+        clara::Opt(resX, "resX")
+        ["-r"]["--resx"]
+        ("grid resolution in x-axis (default is 100)") |
+        clara::Opt(marginScale, "marginScale")
+        ["-m"]["--margin"]
+        ("margin scale around the sdf (default is 0.2)");
 
-	if (inputFileName.empty())
-	{
-		PrintUsage();
-		exit(EXIT_FAILURE);
-	}
+    auto result = parser.parse(clara::Args(argc, argv));
+    if (!result)
+    {
+        std::cerr << "Error in command line: " << result.errorMessage() << '\n';
+        exit(EXIT_FAILURE);
+    }
 
-	if (outputFileName.empty())
-	{
-		PrintUsage();
-		exit(EXIT_FAILURE);
-	}
+    if (showHelp)
+    {
+        std::cout << ToString(parser) << '\n';
+        exit(EXIT_SUCCESS);
+    }
 
-	TriangleMesh3 triMesh;
+    if (inputFileName.empty() || outputFileName.empty())
+    {
+        std::cout << ToString(parser) << '\n';
+        exit(EXIT_FAILURE);
+    }
 
-	std::ifstream objFile(inputFileName.c_str());
-	if (objFile)
-	{
-		printf("Reading obj file %s\n", inputFileName.c_str());
-		triMesh.ReadObj(&objFile);
-		objFile.close();
-	}
-	else
-	{
-		fprintf(stderr, "Failed to open file %s\n", inputFileName.c_str());
-		exit(EXIT_FAILURE);
-	}
+    TriangleMesh3 triMesh;
 
-	BoundingBox3D box = triMesh.BoundingBox();
-	const Vector3D scale(box.GetWidth(), box.GetHeight(), box.GetDepth());
-	box.lowerCorner -= marginScale * scale;
-	box.upperCorner += marginScale * scale;
+    std::ifstream objFile(inputFileName.c_str());
+    if (objFile)
+    {
+        printf("Reading obj file %s\n", inputFileName.c_str());
+        triMesh.ReadObj(&objFile);
+        objFile.close();
+    }
+    else
+    {
+        fprintf(stderr, "Failed to open file %s\n", inputFileName.c_str());
+        exit(EXIT_FAILURE);
+    }
 
-	const size_t resolutionY = static_cast<size_t>(std::ceil(resolutionX * box.GetHeight() / box.GetWidth()));
-	const size_t resolutionZ = static_cast<size_t>(std::ceil(resolutionX * box.GetDepth() / box.GetWidth()));
+    BoundingBox3D box = triMesh.BoundingBox();
+    const Vector3D scale(box.GetWidth(), box.GetHeight(), box.GetDepth());
+    box.lowerCorner -= marginScale * scale;
+    box.upperCorner += marginScale * scale;
 
-	printf(
-		"Vertex-centered grid size: %zu x %zu x %zu\n",
-		resolutionX, resolutionY, resolutionZ);
+    const size_t resolutionY = static_cast<size_t>(std::ceil(resX * box.GetHeight() / box.GetWidth()));
+    const size_t resolutionZ = static_cast<size_t>(std::ceil(resX * box.GetDepth() / box.GetWidth()));
 
-	const double dx = box.GetWidth() / resolutionX;
+    printf(
+        "Vertex-centered grid size: %zu x %zu x %zu\n",
+        resX, resolutionY, resolutionZ);
 
-	VertexCenteredScalarGrid3 grid(
-		resolutionX, resolutionY, resolutionZ,
-		dx, dx, dx,
-		box.lowerCorner.x, box.lowerCorner.y, box.lowerCorner.z);
+    const double dx = box.GetWidth() / resX;
 
-	const BoundingBox3D domain = grid.BoundingBox();
-	printf(
-		"Domain size: [%f, %f, %f] x [%f, %f, %f]\n",
-		domain.lowerCorner.x, domain.lowerCorner.y, domain.lowerCorner.z,
-		domain.upperCorner.x, domain.upperCorner.y, domain.upperCorner.z);
-	printf("Generating SDF...");
+    VertexCenteredScalarGrid3 grid(
+        resX, resolutionY, resolutionZ,
+        dx, dx, dx,
+        box.lowerCorner.x, box.lowerCorner.y, box.lowerCorner.z);
 
-	TriangleMeshToSDF(triMesh, &grid);
+    const BoundingBox3D domain = grid.BoundingBox();
+    printf(
+        "Domain size: [%f, %f, %f] x [%f, %f, %f]\n",
+        domain.lowerCorner.x, domain.lowerCorner.y, domain.lowerCorner.z,
+        domain.upperCorner.x, domain.upperCorner.y, domain.upperCorner.z);
+    printf("Generating SDF...");
 
-	printf("done\n");
+    TriangleMeshToSDF(triMesh, &grid);
 
-	std::ofstream sdfFile(outputFileName.c_str(), std::ofstream::binary);
-	if (sdfFile)
-	{
-		printf("Writing to vertex-centered grid %s\n", outputFileName.c_str());
+    printf("done\n");
 
-		std::vector<uint8_t> buffer;
-		grid.Serialize(&buffer);
-		sdfFile.write(reinterpret_cast<char*>(buffer.data()), buffer.size());
-		sdfFile.close();
-	}
-	else
-	{
-		fprintf(stderr, "Failed to write file %s\n", outputFileName.c_str());
-		exit(EXIT_FAILURE);
-	}
+    std::ofstream sdfFile(outputFileName.c_str(), std::ofstream::binary);
+    if (sdfFile)
+    {
+        printf("Writing to vertex-centered grid %s\n", outputFileName.c_str());
 
-	TriangleMesh3 triMesh2;
-	MarchingCubes(grid.GetConstDataAccessor(), grid.GridSpacing(), grid.Origin(), &triMesh2, 0, DIRECTION_ALL);
+        std::vector<uint8_t> buffer;
+        grid.Serialize(&buffer);
+        sdfFile.write(reinterpret_cast<char*>(buffer.data()), buffer.size());
+        sdfFile.close();
+    }
+    else
+    {
+        fprintf(stderr, "Failed to write file %s\n", outputFileName.c_str());
+        exit(EXIT_FAILURE);
+    }
 
-	SaveTriangleMeshData(triMesh2, outputFileName + "_previz.obj");
+    TriangleMesh3 triMesh2;
+    MarchingCubes(grid.GetConstDataAccessor(), grid.GridSpacing(), grid.Origin(), &triMesh2, 0, DIRECTION_ALL);
 
-	return EXIT_SUCCESS;
+    SaveTriangleMeshData(triMesh2, outputFileName + "_previz.obj");
+
+    return EXIT_SUCCESS;
 }
